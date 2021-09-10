05f2667e1a6dba77d92d8dd3b9404a0b524dc4a8
Daniel Morales <daniel.morales.ext@getronics.com>
2021-09-10 08:21:14 +0200

    Modifing the algorithm of get data and extending others

--- src/Hercules.ED.FormEngine/EditorCV/EditorCV/EditorCV/Models/JsonDataStructure.cs
+++ src/Hercules.ED.FormEngine/EditorCV/EditorCV/EditorCV/Models/JsonDataStructure.cs
@@ -69,6 +69,7 @@ namespace EditorCV.Models
         public string eidRel { get; set; }
         public string eid { get; set; }
         public string ontologyName { get; set; }
+        public string mode { get; set; }
     }
 
 
