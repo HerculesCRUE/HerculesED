bbb44c9d5a37fe2b7e685283cf3b57605d5bac0f
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2021-11-02 15:35:53 +0100

    CAmbios Docker

--- src/EditorCV/Startup.cs
+++ src/EditorCV/Startup.cs
@@ -45,12 +45,11 @@ namespace EditorCV
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EditorCV v1"));
-            }
+
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("v1/swagger.json", "EditorCV v1"));
+
 
             app.UseHttpsRedirection();
 
