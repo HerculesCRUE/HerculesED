a3e36460844890cc672fc2d998004bd6eec06200
Adrián <88077103+AdrianSaavedra-GNOSS@users.noreply.github.com>
2021-12-21 12:44:05 +0100

    Update Rabbit

--- src/Hercules.ED.ExternalSources/WorkerServiceRabbitConsume/WorkerServiceRabbitConsume/WorkerServiceRabbitConsume/WorkerServiceRabbitConsume/Program.cs
+++ src/Hercules.ED.ExternalSources/WorkerServiceRabbitConsume/WorkerServiceRabbitConsume/WorkerServiceRabbitConsume/WorkerServiceRabbitConsume/Program.cs
@@ -1,15 +1,21 @@
 using Gnoss.Web.ReprocessData.Models.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using WorkerServiceRabbitConsume.Middlewares;
 
 namespace WorkerServiceRabbitConsume
 {
     public class Program
     {
+        private string _LogPath;
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
@@ -21,6 +27,7 @@ namespace WorkerServiceRabbitConsume
                 {
                     services.AddSingleton(typeof(ConfigService));
                     services.AddScoped(typeof(ReadRabbitService));
+                    services.AddScoped(typeof(ErrorHandlingMiddleware));
                     services.AddHostedService<Worker>();
                 });
     }
