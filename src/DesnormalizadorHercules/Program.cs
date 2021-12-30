2205d87b99292df44abd28bbab8873cc2bcf169d
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2021-12-30 16:18:13 +0100

    Update Program.cs

--- src/DesnormalizadorHercules/Program.cs
+++ src/DesnormalizadorHercules/Program.cs
@@ -14,7 +14,6 @@ namespace DesnormalizadorHercules
     {
         static void Main()
         {
-            ActualizadorEDMA.DesnormalizarDatosPersona("http://gnoss.com/items/Person_e883a5d8-f7fa-4f9b-aba4-ec6570646b6e_1b1099f0-930c-458a-9ae0-e9c77495d25b");
             bool eliminarDatos = false;
             if (eliminarDatos)
             {
