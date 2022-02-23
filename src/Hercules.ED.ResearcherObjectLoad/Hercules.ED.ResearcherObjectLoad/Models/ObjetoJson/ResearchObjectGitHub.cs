b2633825cd9202cd7998318dbbe533fb63b129ce
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2022-02-23 17:10:52 +0100

    Create ResearchObjectGitHub.cs

---
+++ src/Hercules.ED.ResearcherObjectLoad/Hercules.ED.ResearcherObjectLoad/Models/ObjetoJson/ResearchObjectGitHub.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hercules.ED.ResearcherObjectLoad.Models.ObjetoJson
+{
+    public class ResearchObjectGitHub
+    {
+        public string ID { get; set; }
+        public int? id { get; set; }
+        public string tipo { get; set; }
+        public string titulo { get; set; }
+        public string descripcion { get; set; }
+        public string url { get; set; }
+        public List<string> listaAutores { get; set; }
+        public string fechaCreacion { get; set; }
+        public string fechaActualizacion { get; set; }
+        public Dictionary<string, float> lenguajes { get; set; }
+        public string licencia { get; set; }
+        public int? numReleases { get; set; }
+        public int? numForks { get; set; }
+        public int? numIssues { get; set; }
+        public List<string> etiquetas { get; set; }
+        public List<string> etiquetasEnriquecidas { get; set; }
+        public List<string> categoriasEnriquecidas { get; set; }
+    }
+}
