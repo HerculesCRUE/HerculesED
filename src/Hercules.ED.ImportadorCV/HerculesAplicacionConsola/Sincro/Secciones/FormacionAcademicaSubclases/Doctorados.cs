0b09c1ab35edb550f2869c7df72a2b35aab8c8e8
ManuelGNOSS <manuelboillos@gnoss.com>
2022-03-02 12:47:49 +0100

    Create Doctorados.cs

---
+++ src/Hercules.ED.ImportadorCV/HerculesAplicacionConsola/Sincro/Secciones/FormacionAcademicaSubclases/Doctorados.cs
@@ -0,0 +1,89 @@
+﻿using Gnoss.ApiWrapper;
+using Gnoss.ApiWrapper.ApiModel;
+using Hercules.ED.DisambiguationEngine.Models;
+using HerculesAplicacionConsola.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Gnoss.ApiWrapper.ApiModel.SparqlObject;
+
+namespace HerculesAplicacionConsola.Sincro.Secciones.FormacionAcademicaSubclases
+{
+    class Doctorados : DisambiguableEntity
+    {
+        public string descripcion { get; set; }
+        public string fecha { get; set; }
+
+        private static DisambiguationDataConfig configDescripcion = new DisambiguationDataConfig()
+        {
+            type = DisambiguationDataConfigType.equalsTitle,
+            score = 0.8f
+        };
+
+        private static DisambiguationDataConfig configFecha = new DisambiguationDataConfig()
+        {
+            type = DisambiguationDataConfigType.equalsItem,
+            score = 0.5f,
+            scoreMinus = 0.5f
+        };
+
+        public override List<DisambiguationData> GetDisambiguationData()
+        {
+            List<DisambiguationData> data = new List<DisambiguationData>();
+
+            data.Add(new DisambiguationData()
+            {
+                property = "descripcion",
+                config = configDescripcion,
+                value = descripcion
+            });
+
+            data.Add(new DisambiguationData()
+            {
+                property = "fecha",
+                config = configFecha,
+                value = fecha
+            });
+            return data;
+        }
+
+        public static Dictionary<string, DisambiguableEntity> GetBBDD(ResourceApi pResourceApi, string pCVID, string graph, List<string> propiedadesItem)
+        {
+            //Obtenemos IDS
+            HashSet<string> ids = UtilitySecciones.GetIDS(pResourceApi, pCVID, propiedadesItem);
+
+            Dictionary<string, DisambiguableEntity> resultados = new Dictionary<string, DisambiguableEntity>();
+
+            //Divido la lista en listas de 1.000 elementos
+            List<List<string>> listaListas = UtilitySecciones.SplitList(ids.ToList(), 1000).ToList();
+
+            foreach (List<string> lista in listaListas)
+            {
+                string select = $@"SELECT distinct ?item ?itemTitle ?itemDate ";
+                string where = $@"where {{
+                                        ?item <{Variables.FormacionAcademica.doctoradosTituloTesis}> ?itemTitle . 
+                                        OPTIONAL{{?item <{Variables.FormacionAcademica.doctoradosFechaTitulacion}> ?itemDate }}.
+                                        FILTER(?item in (<{string.Join(">,<", lista)}>))
+                                    }}";
+                //TODO check where valores
+                SparqlObject resultData = pResourceApi.VirtuosoQuery(select, where, graph);
+                foreach (Dictionary<string, Data> fila in resultData.results.bindings)
+                {
+                    Doctorados doctorados = new Doctorados();
+                    doctorados.ID = fila["item"].value;
+                    doctorados.descripcion = fila["itemTitle"].value;
+                    doctorados.fecha = "";
+                    if (fila.ContainsKey("itemDate"))
+                    {
+                        doctorados.fecha = fila["itemDate"].value;
+                    }
+                    resultados.Add(pResourceApi.GetShortGuid(fila["item"].value).ToString(), doctorados);
+                }
+            }
+
+            return resultados;
+        }
+    }
+}
