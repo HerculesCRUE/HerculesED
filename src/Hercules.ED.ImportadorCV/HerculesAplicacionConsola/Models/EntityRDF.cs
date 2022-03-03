113c3cf796733ac49fa6e88ad18fcaf72ee1afea
ManuelGNOSS <manuelboillos@gnoss.com>
2022-03-03 17:13:34 +0100

    Update EntityRDF.cs

--- src/Hercules.ED.ImportadorCV/HerculesAplicacionConsola/Models/EntityRDF.cs
+++ src/Hercules.ED.ImportadorCV/HerculesAplicacionConsola/Models/EntityRDF.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace HerculesAplicacionConsola.Models
+namespace Models
 {
     /// <summary>
     /// Clases usadas para el guardado de una ontología
