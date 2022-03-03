8d8da4cb2e05497bc04a3149248eef8f1bf53227
Zuhaitz Beloki <z.beloki@elhuyar.eus>
2022-03-03 23:26:59 +0100

    more test requests added

--- src/Hercules.ED.Enrichment/ServicioAPI/test/test.py
+++ src/Hercules.ED.Enrichment/ServicioAPI/test/test.py
@@ -4,13 +4,13 @@ import json
 import pdb
 
 
-def test(fpath):
+def test(fpath, descriptor_type):
 
     with open(fpath, 'r') as f:
         jreq = json.load(f)
 
     headers = {'Content-type': 'application/json'}
-    resp = requests.post('http://127.0.0.1:5000/thematic', json=jreq, headers=headers)
+    resp = requests.post(f"http://127.0.0.1:5000/{descriptor_type}", json=jreq, headers=headers)
     if resp.status_code == 200:
         print(json.dumps(resp.json(), indent=4))
 
@@ -19,6 +19,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="wfcount formatutik hitzen zerrenda garbia ateratzen du.")
     parser.add_argument("JSON_PATH", help='Request parameters')
+    parser.add_argument("DESCRIPTOR_TYPE", help='thematic / specific')
     args = parser.parse_args()
 
-    test(args.JSON_PATH)
+    test(args.JSON_PATH, args.DESCRIPTOR_TYPE)
