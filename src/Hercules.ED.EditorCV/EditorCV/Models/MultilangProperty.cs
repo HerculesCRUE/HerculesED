199b4902a5b94966e5ef703f881b5bd8c460bda4
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2022-03-09 10:22:47 +0100

    Create MultilangProperty.cs

---
+++ src/Hercules.ED.EditorCV/EditorCV/Models/MultilangProperty.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EditorCV.Models
+{
+    public class MultilangProperty
+    {
+        public string auxEntityCV { get; set; }
+        public string lang { get; set; }
+        public string value { get; set; }
+    }
+}
