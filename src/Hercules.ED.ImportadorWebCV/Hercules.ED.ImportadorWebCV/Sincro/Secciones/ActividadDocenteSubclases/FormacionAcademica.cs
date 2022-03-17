d51949b36ec3639bbc5b809b4d0849dc1aaf5052
ManuelGNOSS <manuelboillos@gnoss.com>
2022-03-17 08:37:59 +0100

    Update FormacionAcademica.cs

--- src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Sincro/Secciones/ActividadDocenteSubclases/FormacionAcademica.cs
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Sincro/Secciones/ActividadDocenteSubclases/FormacionAcademica.cs
@@ -13,6 +13,7 @@ namespace ImportadorWebCV.Sincro.Secciones.ActividadDocenteSubclases
     {
         public string descripcion { get; set; }
         public string fecha { get; set; }
+        public string entidadRealizacion { get; set; }
 
         private static readonly DisambiguationDataConfig configDescripcion = new DisambiguationDataConfig()
         {
@@ -21,6 +22,13 @@ namespace ImportadorWebCV.Sincro.Secciones.ActividadDocenteSubclases
         };
 
         private static readonly DisambiguationDataConfig configFecha = new DisambiguationDataConfig()
+        {
+            type = DisambiguationDataConfigType.equalsItem,
+            score = 0.5f,
+            scoreMinus = 0.5f
+        };
+
+        private static readonly DisambiguationDataConfig configER = new DisambiguationDataConfig()
         {
             type = DisambiguationDataConfigType.equalsItem,
             score = 0.5f,
@@ -43,6 +51,13 @@ namespace ImportadorWebCV.Sincro.Secciones.ActividadDocenteSubclases
                     property = "fecha",
                     config = configFecha,
                     value = fecha
+                },
+
+                new DisambiguationData()
+                {
+                    property = "entidadRealizacion",
+                    config = configER,
+                    value = entidadRealizacion
                 }
             };
             return data;
@@ -68,10 +83,11 @@ namespace ImportadorWebCV.Sincro.Secciones.ActividadDocenteSubclases
 
             foreach (List<string> lista in listaListas)
             {
-                string select = $@"SELECT distinct ?item ?itemTitle ?itemDate ";
+                string select = $@"SELECT distinct ?item ?itemTitle ?itemDate ?itemER ";
                 string where = $@"where {{
                                         ?item <{Variables.ActividadDocente.formacionAcademicaNombreAsignatura}> ?itemTitle . 
                                         OPTIONAL{{?item <{Variables.ActividadDocente.formacionAcademicaFechaInicio}> ?itemDate }}.
+                                        OPTIONAL{{?item <{Variables.ActividadDocente.formacionAcademicaEntidadRealizacionNombre}> ?itemER }}.
                                         FILTER(?item in (<{string.Join(">,<", lista)}>))
                                     }}";
                 //TODO check where valores
@@ -82,7 +98,8 @@ namespace ImportadorWebCV.Sincro.Secciones.ActividadDocenteSubclases
                     {
                         ID = fila["item"].value,
                         descripcion = fila["itemTitle"].value,
-                        fecha = fila.ContainsKey("itemDate") ? fila["itemDate"].value : ""
+                        fecha = fila.ContainsKey("itemDate") ? fila["itemDate"].value : "",
+                        entidadRealizacion = fila.ContainsKey("itemER") ? fila["itemER"].value : ""
                     };
 
                     resultados.Add(pResourceApi.GetShortGuid(fila["item"].value).ToString(), formacionAcademica);
