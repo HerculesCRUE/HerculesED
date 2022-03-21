3292ccfd7e6daa42bff7b2fd221f41cc56516e9d
ManuelGNOSS <manuelboillos@gnoss.com>
2022-03-21 15:28:39 +0100

    Update FormacionAcademica.cs

--- src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Sincro/Secciones/ActividadDocenteSubclases/FormacionAcademica.cs
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Sincro/Secciones/ActividadDocenteSubclases/FormacionAcademica.cs
@@ -106,7 +106,6 @@ namespace ImportadorWebCV.Sincro.Secciones.ActividadDocenteSubclases
                                         OPTIONAL{{?item <{Variables.ActividadDocente.formacionAcademicaEntidadRealizacionNombre}> ?itemER }}.
                                         FILTER(?item in (<{string.Join(">,<", lista)}>))
                                     }}";
-                //TODO check where valores
                 SparqlObject resultData = pResourceApi.VirtuosoQuery(select, where, graph);
                 foreach (Dictionary<string, Data> fila in resultData.results.bindings)
                 {
