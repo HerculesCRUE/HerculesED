c3990ff3d3a9b05c7e979ae3187c3b1285fe1070
ManuelGNOSS <manuelboillos@gnoss.com>
2022-04-11 15:25:29 +0200

    Update DisambiguationRO.cs

--- src/Hercules.ED.ResearcherObjectLoad/Hercules.ED.ResearcherObjectLoad/Models/DisambiguationObjects/DisambiguationRO.cs
+++ src/Hercules.ED.ResearcherObjectLoad/Hercules.ED.ResearcherObjectLoad/Models/DisambiguationObjects/DisambiguationRO.cs
@@ -129,35 +129,35 @@ namespace Hercules.ED.ResearcherObjectLoad.Models.DisambiguationObjects
             }
         }
 
-        private static DisambiguationDataConfig configTitulo = new DisambiguationDataConfig()
+        private static readonly DisambiguationDataConfig configTitulo = new DisambiguationDataConfig()
         {
             type = DisambiguationDataConfigType.equalsTitle,
             score = 0.8f
         };
 
-        private static DisambiguationDataConfig configDOI = new DisambiguationDataConfig()
+        private static readonly DisambiguationDataConfig configDOI = new DisambiguationDataConfig()
         {
             type = DisambiguationDataConfigType.equalsIdentifiers
         };
 
-        private static DisambiguationDataConfig configIdFigshare = new DisambiguationDataConfig()
-        {
-            type = DisambiguationDataConfigType.equalsIdentifiers
-        };
-
-
-        private static DisambiguationDataConfig configIdGitHub = new DisambiguationDataConfig()
-        {
-            type = DisambiguationDataConfigType.equalsIdentifiers
-        };
-
-        private static DisambiguationDataConfig configIdZenodo = new DisambiguationDataConfig()
+        private static readonly DisambiguationDataConfig configIdFigshare = new DisambiguationDataConfig()
         {
             type = DisambiguationDataConfigType.equalsIdentifiers
         };
 
 
-        private static DisambiguationDataConfig configAutores = new DisambiguationDataConfig()
+        private static readonly DisambiguationDataConfig configIdGitHub = new DisambiguationDataConfig()
+        {
+            type = DisambiguationDataConfigType.equalsIdentifiers
+        };
+
+        private static readonly DisambiguationDataConfig configIdZenodo = new DisambiguationDataConfig()
+        {
+            type = DisambiguationDataConfigType.equalsIdentifiers
+        };
+
+
+        private static readonly DisambiguationDataConfig configAutores = new DisambiguationDataConfig()
         {
             type = DisambiguationDataConfigType.equalsItemList,
             score = 0.5f
