19093806359f10c45fc34e0b277a174bb7094c75
ManuelGNOSS <manuelboillos@gnoss.com>
2022-04-12 09:22:21 +0200

    Simplificación código con genéricos

--- src/Hercules.ED.ResearcherObjectLoad/Hercules.ED.ResearcherObjectLoad/Utils/Utility.cs
+++ src/Hercules.ED.ResearcherObjectLoad/Hercules.ED.ResearcherObjectLoad/Utils/Utility.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Hercules.ED.ResearcherObjectLoad.Models.ObjetoJson;
 using Hercules.ED.ResearcherObjectLoad.Models;
+using ResearchobjectOntology;
 
 namespace Hercules.ED.ResearcherObjectLoad.Utils
 {
@@ -26,11 +27,11 @@ namespace Hercules.ED.ResearcherObjectLoad.Utils
             }
         }
 
-        public static ResearchobjectOntology.ResearchObject ConstruirRO(string pTipo, object pResearchObject,
+        public static ResearchObject ConstruirRO(string pTipo, object pResearchObject,
             Dictionary<string, string> pDicAreasBroader, Dictionary<string, string> pDicAreasNombre,
             object pResearchObject_b = null)
         {
-            ResearchobjectOntology.ResearchObject ro = new ResearchobjectOntology.ResearchObject();
+            ResearchObject ro = new ResearchObject();
 
             // Estado de validación (IsValidated)
             ro.Roh_isValidated = true;
@@ -71,6 +72,175 @@ namespace Hercules.ED.ResearcherObjectLoad.Utils
             return ro;
         }
 
+        /// <summary>
+        /// Metodo generico para insertar el valor de la licencia en las clases hijas de RO_JSON
+        /// </summary>
+        /// <typeparam name="T">T:RO_JSON</typeparam>
+        /// <param name="researchObject"></param>
+        /// <param name="researchObject_B"></param>
+        /// <param name="ro"></param>
+        public static void Licencia<T>(T researchObject, T researchObject_B, ResearchObject ro) where T : RO_JSON
+        {
+            if (!string.IsNullOrEmpty(researchObject.licencia))
+            {
+                ro.Dct_license = researchObject.licencia;
+
+                if (researchObject_B != null && !string.IsNullOrEmpty(researchObject_B.licencia) && string.IsNullOrEmpty(ro.Dct_license))
+                {
+                    ro.Dct_license = researchObject_B.licencia;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Metodo generico para insertar el valor del titulo en las clases hijas de RO_JSON
+        /// </summary>
+        /// <typeparam name="T">T:RO_JSON</typeparam>
+        /// <param name="researchObject"></param>
+        /// <param name="researchObject_B"></param>
+        /// <param name="ro"></param>
+        public static void Titulo<T>(T researchObject, T researchObject_B, ResearchObject ro) where T : RO_JSON
+        {
+            if (!string.IsNullOrEmpty(researchObject.titulo))
+            {
+                ro.Roh_title = researchObject.titulo;
+
+                if (researchObject_B != null && !string.IsNullOrEmpty(researchObject_B.titulo) && string.IsNullOrEmpty(ro.Roh_title))
+                {
+                    ro.Roh_title = researchObject_B.titulo;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Metodo generico para insertar el valor de la descripción en las clases hijas de RO_JSON
+        /// </summary>
+        /// <typeparam name="T">T:RO_JSON</typeparam>
+        /// <param name="researchObject"></param>
+        /// <param name="researchObject_B"></param>
+        /// <param name="ro"></param>
+        public static void Descripcion<T>(T researchObject, T researchObject_B, ResearchObject ro) where T : RO_JSON
+        {
+            if (!string.IsNullOrEmpty(researchObject.titulo))
+            {
+                ro.Bibo_abstract = researchObject.descripcion;
+
+                if (researchObject_B != null && !string.IsNullOrEmpty(researchObject_B.descripcion) && string.IsNullOrEmpty(ro.Bibo_abstract))
+                {
+                    ro.Bibo_abstract = researchObject_B.descripcion;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Metodo generico para insertar el valor de la URL en las clases hijas de RO_JSON
+        /// </summary>
+        /// <typeparam name="T">T:RO_JSON</typeparam>
+        /// <param name="researchObject"></param>
+        /// <param name="researchObject_B"></param>
+        /// <param name="ro"></param>
+        public static void URL<T>(T researchObject, T researchObject_B, ResearchObject ro) where T : RO_JSON
+        {
+            if (!string.IsNullOrEmpty(researchObject.url))
+            {
+                ro.Vcard_url = researchObject.url;
+
+                if (researchObject_B != null && !string.IsNullOrEmpty(researchObject_B.url) && string.IsNullOrEmpty(ro.Vcard_url))
+                {
+                    ro.Vcard_url = researchObject_B.url;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Metodo generico para insertar el valor de las etiquetas enriquecidas en las clases hijas de RO_JSON
+        /// </summary>
+        /// <typeparam name="T">T:RO_JSON</typeparam>
+        /// <param name="researchObject"></param>
+        /// <param name="researchObject_B"></param>
+        /// <param name="ro"></param>
+        public static void EtiquetasEnriquecidas<T>(T researchObject, T researchObject_B, ResearchObject ro) where T : RO_JSON
+        {
+            if (researchObject.etiquetasEnriquecidas != null && researchObject.etiquetasEnriquecidas.Any())
+            {
+                ro.Roh_enrichedKeywords = researchObject.etiquetasEnriquecidas;
+
+                if (researchObject_B != null && researchObject_B.etiquetasEnriquecidas != null && researchObject_B.etiquetasEnriquecidas.Any())
+                {
+                    ro.Roh_enrichedKeywords = researchObject_B.etiquetasEnriquecidas;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Metodo generico para insertar el valor de las categorias enriquecidas en las clases hijas de RO_JSON
+        /// </summary>
+        /// <typeparam name="T">T:RO_JSON</typeparam>
+        /// <param name="researchObject"></param>
+        /// <param name="researchObject_B"></param>
+        /// <param name="ro"></param>
+        public static void CategoriasEnriquecidas<T>(T researchObject, T researchObject_B, 
+            Dictionary<string, string> pDicAreasNombre, Dictionary<string, string> pDicAreasBroader, ResearchObject ro) where T : RO_JSON
+        {
+            HashSet<string> listaIDs = new HashSet<string>();
+            if (researchObject.categoriasEnriquecidas != null && researchObject.categoriasEnriquecidas.Count > 0)
+            {
+                ro.Roh_enrichedKnowledgeArea = new List<CategoryPath>();
+                foreach (string area in researchObject.categoriasEnriquecidas)
+                {
+                    if (pDicAreasNombre.ContainsKey(area.ToLower()))
+                    {
+                        CategoryPath categoria = new CategoryPath();
+                        categoria.IdsRoh_categoryNode = new List<string>();
+                        categoria.IdsRoh_categoryNode.Add(pDicAreasNombre[area.ToLower()]);
+                        string idHijo = pDicAreasNombre[area.ToLower()];
+                        string idHijoAux = idHijo;
+                        if (!listaIDs.Contains(idHijo))
+                        {
+                            while (!idHijo.EndsWith(".0.0.0"))
+                            {
+                                categoria.IdsRoh_categoryNode.Add(pDicAreasBroader[idHijo]);
+                                idHijo = pDicAreasBroader[idHijo];
+                            }
+                            if (categoria.IdsRoh_categoryNode.Count > 0)
+                            {
+                                ro.Roh_enrichedKnowledgeArea.Add(categoria);
+                            }
+                        }
+                        listaIDs.Add(idHijoAux);
+                    }
+                }
+
+                if (researchObject_B != null && researchObject_B.categoriasEnriquecidas != null && researchObject_B.categoriasEnriquecidas.Any())
+                {
+                    ro.Roh_enrichedKnowledgeArea = new List<CategoryPath>();
+                    foreach (string area in researchObject_B.categoriasEnriquecidas)
+                    {
+                        if (pDicAreasNombre.ContainsKey(area.ToLower()))
+                        {
+                            CategoryPath categoria = new CategoryPath();
+                            categoria.IdsRoh_categoryNode = new List<string>();
+                            categoria.IdsRoh_categoryNode.Add(pDicAreasNombre[area.ToLower()]);
+                            string idHijo = pDicAreasNombre[area.ToLower()];
+                            string idHijoAux = idHijo;
+                            if (!listaIDs.Contains(idHijo))
+                            {
+                                while (!idHijo.EndsWith(".0.0.0"))
+                                {
+                                    categoria.IdsRoh_categoryNode.Add(pDicAreasBroader[idHijo]);
+                                    idHijo = pDicAreasBroader[idHijo];
+                                }
+                                if (categoria.IdsRoh_categoryNode.Count > 0)
+                                {
+                                    ro.Roh_enrichedKnowledgeArea.Add(categoria);
+                                }
+                            }
+                            listaIDs.Add(idHijoAux);
+                        }
+                    }
+                }
+            }
+        }
 
     }
 }
