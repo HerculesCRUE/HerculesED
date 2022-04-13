b7fa1564d9f911d012b0588cb1859d33bb20c615
ManuelGNOSS <manuelboillos@gnoss.com>
2022-04-13 10:11:06 +0200

    Update AccionesGuardado.cs

--- src/Hercules.ED.EditorCV/EditorCV/Models/AccionesGuardado.cs
+++ src/Hercules.ED.EditorCV/EditorCV/Models/AccionesGuardado.cs
@@ -16,6 +16,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using static Gnoss.ApiWrapper.ApiModel.SparqlObject;
 
