a2e685d22a6aaea38cbc60405fc4a2704ccc1b14
ManuelGNOSS <manuelboillos@gnoss.com>
2022-05-05 13:09:55 +0200

    Update Utility.cs

--- src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Utils/Utility.cs
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Utils/Utility.cs
@@ -770,7 +770,8 @@ namespace Utils
                                 {
                                     StringBuilder sbUnion = new StringBuilder();
                                     sbUnion.AppendLine("				?personID <http://xmlns.com/foaf/0.1/name> ?name.");
-                                    sbUnion.AppendLine($@"				?name bif:contains ""'{word}'"" BIND({score} as ?num) ");
+                                    sbUnion.AppendLine($@"				{FilterWordComplete(word, "name")} BIND({score} as ?num) ");
+                                    //sbUnion.AppendLine($@"				?name bif:contains ""'{word}'"" BIND({score} as ?num) ");
                                     unions.Add(sbUnion.ToString());
                                 }
                             }
@@ -824,6 +825,24 @@ namespace Utils
             }
 
             return diccionarioPersonasFirma;
+        }
+
+        public static string FilterWordComplete(string pWord, string pVar)
+        {
+            Dictionary<string, string> listaReemplazos = new Dictionary<string, string>();
+            listaReemplazos["a"] = "aáàä";
+            listaReemplazos["e"] = "eéèë";
+            listaReemplazos["i"] = "iíìï";
+            listaReemplazos["o"] = "oóòö";
+            listaReemplazos["u"] = "uúùü";
+            listaReemplazos["n"] = "nñ";
+            listaReemplazos["c"] = "cç";
+            foreach (string caracter in listaReemplazos.Keys)
+            {
+                pWord = pWord.Replace(caracter, $"[{listaReemplazos[caracter]}]");
+            }
+            string filter = @$"FILTER ( regex(?{pVar},""(^| ){pWord}($| )"", ""i""))";
+            return filter;
         }
 
         /// <summary>
