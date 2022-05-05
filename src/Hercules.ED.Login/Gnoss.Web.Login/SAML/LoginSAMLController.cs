f65b58dedfe7f5567def93a60778d588b1315b95
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2022-05-05 11:10:07 +0200

    Update LoginSAMLController.cs

--- src/Hercules.ED.Login/Gnoss.Web.Login/SAML/LoginSAMLController.cs
+++ src/Hercules.ED.Login/Gnoss.Web.Login/SAML/LoginSAMLController.cs
@@ -63,7 +63,7 @@ namespace Gnoss.Web.Login.SAML
                     //Si no hay usuario redirigimos al login                     
                     var x = HttpContext.Request;
                     Response.Redirect("http://"+HttpContext.Request.PathBase.ToString() +"/"+ HttpContext.Request.PathBase.ToString());
-                    Response.Redirect(Url.Content(@"~/Auth/Login") + "?returnUrl=" + returnUrl +"&token="+ token);
+                    //Response.Redirect(Url.Content(@"~/Auth/Login") + "?returnUrl=" + returnUrl +"&token="+ token);
                     
                 }
             }
