1f8c596f072cb376cdd47ca2e724e2ed552ec007
ManuelGNOSS <manuelboillos@gnoss.com>
2022-05-06 14:16:56 +0200

    Subclases Actividad Cientifica

---
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/ActividadCientificaSubclases/ComitesCTA.cs
@@ -0,0 +1,16 @@
+﻿using ImportadorWebCV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hercules.ED.ImportadorWebCV.Exporta.Secciones.ActividadCientificaSubclases
+{
+    public class ComitesCTA : SeccionBase
+    {
+        public ComitesCTA(cvnRootResultBean cvn, string cvID) : base(cvn, cvID)
+        {
+
+        }
+    }
+}
---
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/ActividadCientificaSubclases/PeriodosActividad.cs
@@ -0,0 +1,16 @@
+﻿using ImportadorWebCV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hercules.ED.ImportadorWebCV.Exporta.Secciones.ActividadCientificaSubclases
+{
+    public class PeriodosActividad:SeccionBase
+    {
+        public PeriodosActividad(cvnRootResultBean cvn, string cvID) : base(cvn, cvID)
+        {
+
+        }
+    }
+}
