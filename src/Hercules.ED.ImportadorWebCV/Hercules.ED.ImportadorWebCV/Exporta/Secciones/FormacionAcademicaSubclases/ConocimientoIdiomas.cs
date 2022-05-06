2867b8e1110352b642ee22a43c25865a133d6f8e
ManuelGNOSS <manuelboillos@gnoss.com>
2022-05-06 14:05:32 +0200

    Subclases Formacion Academica

---
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/FormacionAcademicaSubclases/ConocimientoIdiomas.cs
@@ -0,0 +1,16 @@
+﻿using ImportadorWebCV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hercules.ED.ImportadorWebCV.Exporta.Secciones.FormacionAcademicaSubclases
+{
+    public class ConocimientoIdiomas:SeccionBase
+    {
+        public ConocimientoIdiomas(cvnRootResultBean cvn, string cvID) : base(cvn, cvID)
+        {
+
+        }
+    }
+}
---
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/FormacionAcademicaSubclases/EstudiosCiclos.cs
@@ -0,0 +1,16 @@
+﻿using ImportadorWebCV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hercules.ED.ImportadorWebCV.Exporta.Secciones.FormacionAcademicaSubclases
+{
+    public class EstudiosCiclos:SeccionBase
+    {
+        public EstudiosCiclos(cvnRootResultBean cvn, string cvID) : base(cvn, cvID)
+        {
+
+        }
+    }
+}
