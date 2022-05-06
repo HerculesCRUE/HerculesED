f4a7f7504068c666984c7fd8c4e6959d202ebd57
ManuelGNOSS <manuelboillos@gnoss.com>
2022-05-06 14:08:46 +0200

    Subclases Actividad Docente

---
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/ActividadDocenteSubclases/ParticipacionProyectosInnovacionDocente.cs
@@ -0,0 +1,16 @@
+﻿using ImportadorWebCV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hercules.ED.ImportadorWebCV.Exporta.Secciones.ActividadDocenteSubclases
+{
+    public class ParticipacionProyectosInnovacionDocente:SeccionBase
+    {
+        public ParticipacionProyectosInnovacionDocente(cvnRootResultBean cvn, string cvID) : base(cvn, cvID)
+        {
+
+        }
+    }
+}
---
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/ActividadDocenteSubclases/PublicacionesDocentes.cs
@@ -0,0 +1,16 @@
+﻿using ImportadorWebCV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hercules.ED.ImportadorWebCV.Exporta.Secciones.ActividadDocenteSubclases
+{
+    public class PublicacionesDocentes:SeccionBase
+    {
+        public PublicacionesDocentes(cvnRootResultBean cvn, string cvID) : base(cvn, cvID)
+        {
+
+        }
+    }
+}
