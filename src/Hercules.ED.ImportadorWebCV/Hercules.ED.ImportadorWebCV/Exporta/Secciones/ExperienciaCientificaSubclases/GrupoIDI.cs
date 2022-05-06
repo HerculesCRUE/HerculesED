9e5c6932e9fcccf3a1b69581d9f1f3deb2429441
ManuelGNOSS <manuelboillos@gnoss.com>
2022-05-06 14:06:51 +0200

    Subclases Experiencia Cientifica

---
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/ExperienciaCientificaSubclases/GrupoIDI.cs
@@ -0,0 +1,16 @@
+﻿using ImportadorWebCV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hercules.ED.ImportadorWebCV.Exporta.Secciones.ExperienciaCientificaSubclases
+{
+    public class GrupoIDI:SeccionBase
+    {
+        public GrupoIDI(cvnRootResultBean cvn, string cvID) : base(cvn, cvID)
+        {
+
+        }
+    }
+}
