160bebafe7ec723cff5f2e6d97f1b96b52e8c958
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2022-05-09 12:23:32 +0200

    Update LoginSAMLController.cs

--- src/Hercules.ED.Login/Gnoss.Web.Login/SAML/LoginSAMLController.cs
+++ src/Hercules.ED.Login/Gnoss.Web.Login/SAML/LoginSAMLController.cs
@@ -76,7 +76,7 @@ namespace Gnoss.Web.Login.SAML
 
         private string LoguearUsuario(ClaimsPrincipal pUser,string pReturnUrl,string pToken)
         {
-            string email = "skarmeta@um.es";
+            string email = "";
 
             mCommunityApi.Log.Info("3.-numClaims:" + pUser.Claims.ToList());
 
@@ -86,7 +86,7 @@ namespace Gnoss.Web.Login.SAML
                 mCommunityApi.Log.Info("4.-CLAIM TYPE: '" + claim.Type + "' CLAIMVALUE: '" + claim.Value.Trim().ToLower() + "'");
                 if(claim.Type== "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier")
                 {
-                    //email = claim.Value.Trim();
+                    email = claim.Value.Trim();
                 }            
             }
 
