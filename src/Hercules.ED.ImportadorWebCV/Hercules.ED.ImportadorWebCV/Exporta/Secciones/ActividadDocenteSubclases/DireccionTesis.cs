6be9cb5e3a6d0c26eae6cd38ec4348bcaa2c6318
ManuelGNOSS <manuelboillos@gnoss.com>
2022-05-09 17:13:00 +0200

    Update DireccionTesis.cs

--- src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/ActividadDocenteSubclases/DireccionTesis.cs
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/ActividadDocenteSubclases/DireccionTesis.cs
@@ -1,16 +1,46 @@
-﻿using ImportadorWebCV;
+﻿using ExportadorWebCV.Utils;
+using ImportadorWebCV;
+using Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace ImportadorWebCV.Exporta.Secciones.ActividadDocenteSubclases
 {
     public class DireccionTesis:SeccionBase
     {
+        private List<string> propiedadesItem = new List<string>() { "http://w3id.org/roh/teachingExperience",
+            "http://w3id.org/roh/thesisSupervisions", "http://vivoweb.org/ontology/core#relatedBy" };
+        private string graph = "thesissupervision";
+
         public DireccionTesis(cvnRootResultBean cvn, string cvID) : base(cvn, cvID)
         {
 
+        }
+
+        public void ExportaDireccionTesis(Entity entity, string seccion, [Optional] List<string> secciones, [Optional] bool preimportar)
+        {
+            List<CvnItemBean> listado = new List<CvnItemBean>();
+            List<string> listadoIdentificadores = UtilityExportar.GetListadoEntidades(mResourceApi, propiedadesItem, mCvID);
+            Dictionary<string, Entity> listaEntidadesSP = GetListLoadedEntity(listadoIdentificadores, graph);
+            foreach (KeyValuePair<string, Entity> keyValue in listaEntidadesSP)
+            {
+                CvnItemBean itemBean = new CvnItemBean()
+                {
+                    Code = "030.040.000.000",
+                    Items = new List<CVNObject>()
+                };
+
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ActividadDocente.cursosSeminariosTipoEvento),
+                    "030.040.000.010", keyValue.Value);
+
+                listado.Add(itemBean);
+            }
+
+            //Añado en el cvnRootResultBean los items que forman parte del listado
+            UtilityExportar.AniadirItems(mCvn, listado);
         }
     }
 }
