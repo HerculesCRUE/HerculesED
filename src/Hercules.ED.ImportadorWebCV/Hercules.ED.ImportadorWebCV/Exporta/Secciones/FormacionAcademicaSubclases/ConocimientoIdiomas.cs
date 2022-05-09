1fc0a6d6ed0aa6d05eb1b8516b3b25e1661537ac
ManuelGNOSS <manuelboillos@gnoss.com>
2022-05-09 16:34:48 +0200

    Update ConocimientoIdiomas.cs

--- src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/FormacionAcademicaSubclases/ConocimientoIdiomas.cs
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/FormacionAcademicaSubclases/ConocimientoIdiomas.cs
@@ -1,4 +1,5 @@
-﻿using ImportadorWebCV;
+﻿using ExportadorWebCV.Utils;
+using ImportadorWebCV;
 using Models;
 using System;
 using System.Collections.Generic;
@@ -8,14 +9,50 @@ using System.Threading.Tasks;
 
 namespace ImportadorWebCV.Exporta.Secciones.FormacionAcademicaSubclases
 {
-    public class ConocimientoIdiomas:SeccionBase
+    public class ConocimientoIdiomas : SeccionBase
     {
+        private List<string> propiedadesItem = new List<string>() { "http://w3id.org/roh/qualifications",
+            "http://w3id.org/roh/languageSkills", "http://vivoweb.org/ontology/core#relatedBy" };
+        private string graph = "languagecertificate";
+
         public ConocimientoIdiomas(cvnRootResultBean cvn, string cvID) : base(cvn, cvID)
         {
 
         }
 
         public void ExportaConocimientoIdiomas(Entity entity, string seccion, [Optional] List<string> secciones, [Optional] bool preimportar)
-        { }
+        {
+            List<CvnItemBean> listado = new List<CvnItemBean>();
+            List<string> listadoIdentificadores = UtilityExportar.GetListadoEntidades(mResourceApi, propiedadesItem, mCvID);
+            Dictionary<string, Entity> listaEntidadesSP = GetListLoadedEntity(listadoIdentificadores, graph);
+            foreach (KeyValuePair<string, Entity> keyValue in listaEntidadesSP)
+            {
+                CvnItemBean itemBean = new CvnItemBean()
+                {
+                    Code = "020.060.000.000",
+                    Items = new List<CVNObject>()
+                };
+
+                UtilityExportar.AddLanguage(itemBean, UtilityExportar.EliminarRDF(Variables.FormacionAcademica.conocimientoIdiomasIdioma),
+                    "020.060.000.010", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.FormacionAcademica.conocimientoIdiomasComprensionAuditiva),
+                    "020.060.000.120", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.FormacionAcademica.conocimientoIdiomasComprensionLectura),
+                    "020.060.000.130", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.FormacionAcademica.conocimientoIdiomasInteraccionOral),
+                    "020.060.000.140", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.FormacionAcademica.conocimientoIdiomasExpresionOral),
+                    "020.060.000.150", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.FormacionAcademica.conocimientoIdiomasExpresionEscrita),
+                    "020.060.000.160", keyValue.Value);
+
+
+
+                listado.Add(itemBean);
+            }
+
+            //Añado en el cvnRootResultBean los items que forman parte del listado
+            UtilityExportar.AniadirItems(mCvn, listado);
         }
+    }
 }
