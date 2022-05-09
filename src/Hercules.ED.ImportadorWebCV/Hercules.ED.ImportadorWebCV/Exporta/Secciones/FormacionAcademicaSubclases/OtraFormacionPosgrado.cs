7e75317dbf0853189978729fc1c84bde610d36e9
ManuelGNOSS <manuelboillos@gnoss.com>
2022-05-09 16:35:15 +0200

    Update OtraFormacionPosgrado.cs

--- src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/FormacionAcademicaSubclases/OtraFormacionPosgrado.cs
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/FormacionAcademicaSubclases/OtraFormacionPosgrado.cs
@@ -1,4 +1,5 @@
-﻿using ImportadorWebCV;
+﻿using ExportadorWebCV.Utils;
+using ImportadorWebCV;
 using Models;
 using System;
 using System.Collections.Generic;
@@ -10,13 +11,67 @@ namespace ImportadorWebCV.Exporta.Secciones.FormacionAcademicaSubclases
 {
     public class OtraFormacionPosgrado : SeccionBase
     {
+        private List<string> propiedadesItem = new List<string>() { "http://w3id.org/roh/qualifications",
+            "http://w3id.org/roh/postgraduates", "http://vivoweb.org/ontology/core#relatedBy" };
+        private string graph = "academicdegree";
+
         public OtraFormacionPosgrado(cvnRootResultBean cvn, string cvID) : base(cvn, cvID)
         {
 
         }
 
         public void ExportaOtraFormacionPosgrado(Entity entity, string seccion, [Optional] List<string> secciones, [Optional] bool preimportar)
-        { }
+        {
+            List<CvnItemBean> listado = new List<CvnItemBean>();
+            List<string> listadoIdentificadores = UtilityExportar.GetListadoEntidades(mResourceApi, propiedadesItem, mCvID);
+            Dictionary<string, Entity> listaEntidadesSP = GetListLoadedEntity(listadoIdentificadores, graph);
+            foreach (KeyValuePair<string, Entity> keyValue in listaEntidadesSP)
+            {
+                CvnItemBean itemBean = new CvnItemBean()
+                {
+                    Code = "020.010.030.000",
+                    Items = new List<CVNObject>()
+                };
+
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.FormacionAcademica.otraFormacionTipoFormacion),
+                    "020.010.030.060", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.FormacionAcademica.otraFormacionPaisEntidadTitulacion),
+                    "020.010.030.040", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.FormacionAcademica.otraFormacionCCAAEntidadTitulacion),
+                    "020.010.030.050", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.FormacionAcademica.otraFormacionCiudadEntidadTitulacion),
+                    "020.010.030.070", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnDateDayMonthYear(itemBean, UtilityExportar.EliminarRDF(Variables.FormacionAcademica.otraFormacionFechaTitulacion),
+                    "020.010.030.120", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.FormacionAcademica.otraFormacionCalificacionObtenida),
+                    "020.010.030.130", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnBoolean(itemBean, UtilityExportar.EliminarRDF(Variables.FormacionAcademica.otraFormacionTituloHomologado),
+                    "020.010.030.150", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnDateDayMonthYear(itemBean, UtilityExportar.EliminarRDF(Variables.FormacionAcademica.otraFormacionFechaHomologacion),
+                    "020.010.030.160", keyValue.Value);
+
+                //Titulacion posgrado
+                UtilityExportar.AddCvnItemBeanCvnTitleBean(itemBean, UtilityExportar.EliminarRDF(Variables.FormacionAcademica.otraFormacionTituloPosgrado),
+                     UtilityExportar.EliminarRDF(Variables.FormacionAcademica.otraFormacionTituloPosgradoNombre),
+                     "020.010.030.020", keyValue.Value);
+
+                //Entidad titulacion
+                UtilityExportar.AddCvnItemBeanCvnEntityBean(itemBean, UtilityExportar.EliminarRDF(Variables.FormacionAcademica.otraFormacionEntidadTitulacionNombre),
+                    "020.010.030.080",entity);
+                UtilityExportar.AddCvnItemBeanCvnEntityBean(itemBean, UtilityExportar.EliminarRDF(Variables.FormacionAcademica.otraFormacionFacultadEscuela),
+                    "020.010.030.140",entity);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.FormacionAcademica.otraFormacionTipoEntidadTitulacion),
+                    "020.010.030.100", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.FormacionAcademica.otraFormacionTipoEntidadTitulacionOtros),
+                    "020.010.030.110", keyValue.Value);
+
+
+                listado.Add(itemBean);
+            }
+
+            //Añado en el cvnRootResultBean los items que forman parte del listado
+            UtilityExportar.AniadirItems(mCvn, listado);
+        }
 
     }
 }
