2d10e06a2959f46f6b21d2ce18e9ea16e2bdbcab
ManuelGNOSS <manuelboillos@gnoss.com>
2022-05-10 09:18:17 +0200

    Update DireccionTesis.cs

--- src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/ActividadDocenteSubclases/DireccionTesis.cs
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/ActividadDocenteSubclases/DireccionTesis.cs
@@ -33,8 +33,59 @@ namespace ImportadorWebCV.Exporta.Secciones.ActividadDocenteSubclases
                     Items = new List<CVNObject>()
                 };
 
-                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ActividadDocente.cursosSeminariosTipoEvento),
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ActividadDocente.direccionTesisTipoProyecto),
                     "030.040.000.010", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ActividadDocente.direccionTesisTipoProyectoOtros),
+                    "030.040.000.020", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ActividadDocente.direccionTesisTituloTrabajo),
+                    "030.040.000.030", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ActividadDocente.direccionTesisPaisEntidadRealizacion),
+                    "030.040.000.040", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ActividadDocente.direccionTesisCCAAEntidadRealizacion),
+                    "030.040.000.050", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ActividadDocente.direccionTesisCiudadEntidadRealizacion),
+                    "030.040.000.070", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnDateDayMonthYear(itemBean, UtilityExportar.EliminarRDF(Variables.ActividadDocente.direccionTesisFechaDefensa),
+                    "030.040.000.140", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ActividadDocente.direccionTesisCalificacionObtenida),
+                    "030.040.000.150", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnDateDayMonthYear(itemBean, UtilityExportar.EliminarRDF(Variables.ActividadDocente.direccionTesisFechaMencionDoctUE),
+                    "030.040.000.160", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnBoolean(itemBean, UtilityExportar.EliminarRDF(Variables.ActividadDocente.direccionTesisMencionCalidad),
+                    "030.040.000.170", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnBoolean(itemBean, UtilityExportar.EliminarRDF(Variables.ActividadDocente.direccionTesisDoctoradoUE),
+                    "030.040.000.190", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnDateDayMonthYear(itemBean, UtilityExportar.EliminarRDF(Variables.ActividadDocente.direccionTesisFechaMencionCalidad),
+                    "030.040.000.200", keyValue.Value);
+
+                //Palabras clave TODO
+
+
+                //Entidad realizacion
+                UtilityExportar.AddCvnItemBeanCvnEntityBean(itemBean, UtilityExportar.EliminarRDF(Variables.ActividadDocente.direccionTesisEntidadRealizacionNombre),
+                    "030.040.000.080", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ActividadDocente.direccionTesisTipoEntidadRealizacion),
+                    "030.040.000.100", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ActividadDocente.direccionTesisTipoEntidadRealizacionOtros),
+                    "030.040.000.110", keyValue.Value);
+
+                //Alumno
+                Dictionary<string, string> listadoPropiedadesAlumno = new Dictionary<string, string>();
+                listadoPropiedadesAlumno.Add("Firma", UtilityExportar.EliminarRDF(Variables.ActividadDocente.direccionTesisAlumnoFirma));
+                listadoPropiedadesAlumno.Add("Nombre", UtilityExportar.EliminarRDF(Variables.ActividadDocente.direccionTesisAlumnoNombre));
+                listadoPropiedadesAlumno.Add("PrimerApellido", UtilityExportar.EliminarRDF(Variables.ActividadDocente.direccionTesisAlumnoPrimerApellido));
+                listadoPropiedadesAlumno.Add("SegundoApellido", UtilityExportar.EliminarRDF(Variables.ActividadDocente.direccionTesisAlumnoSegundoApellido));
+
+                UtilityExportar.AddCvnItemBeanCvnAuthorBean(itemBean, listadoPropiedadesAlumno,"030.040.000.180",keyValue.Value);
+
+                //Codirectores
+                Dictionary<string, string> listadoPropiedadesCodirector = new Dictionary<string, string>();
+                listadoPropiedadesCodirector.Add("Firma", UtilityExportar.EliminarRDF(Variables.ActividadDocente.direccionTesisCodirectorTesisFirma));
+                listadoPropiedadesCodirector.Add("Nombre", UtilityExportar.EliminarRDF(Variables.ActividadDocente.direccionTesisCodirectorTesisNombre));
+                listadoPropiedadesCodirector.Add("PrimerApellido", UtilityExportar.EliminarRDF(Variables.ActividadDocente.direccionTesisCodirectorTesisPrimerApellido));
+                listadoPropiedadesCodirector.Add("SegundoApellido", UtilityExportar.EliminarRDF(Variables.ActividadDocente.direccionTesisCodirectorTesisSegundoApellido));
+
+                UtilityExportar.AddCvnItemBeanCvnAuthorBeanList(itemBean, listadoPropiedadesCodirector, "030.040.000.180", keyValue.Value);
 
                 listado.Add(itemBean);
             }
