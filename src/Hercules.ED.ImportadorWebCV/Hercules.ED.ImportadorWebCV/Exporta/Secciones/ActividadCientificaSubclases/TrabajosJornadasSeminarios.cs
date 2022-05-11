75556a95ac250303d69bbc9e9683ed36be1ce092
Mario Heras <marioheras@gnoss.com>
2022-05-11 10:34:50 +0200

    Update TrabajosJornadasSeminarios.cs

--- src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/ActividadCientificaSubclases/TrabajosJornadasSeminarios.cs
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/ActividadCientificaSubclases/TrabajosJornadasSeminarios.cs
@@ -1,16 +1,39 @@
-﻿using ImportadorWebCV;
+﻿using ExportadorWebCV.Utils;
+using ImportadorWebCV;
+using Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace ImportadorWebCV.Exporta.Secciones.ActividadCientificaSubclases
 {
     public class TrabajosJornadasSeminarios:SeccionBase
     {
+        List<string> propiedadesItem = new List<string>() { "http://w3id.org/roh/scientificActivity", 
+            "http://w3id.org/roh/worksSubmittedSeminars", "http://vivoweb.org/ontology/core#relatedBy" };
+        string graph = "document";
         public TrabajosJornadasSeminarios(cvnRootResultBean cvn, string cvID) : base(cvn, cvID)
         {
+        }
+        public void ExportaConsejos(Entity entity, string seccion, [Optional] List<string> secciones, [Optional] bool preimportar)
+        {
+            List<CvnItemBean> listado = new List<CvnItemBean>();
+            List<string> listadoIdentificadores = UtilityExportar.GetListadoEntidades(mResourceApi, propiedadesItem, mCvID);
+            Dictionary<string, Entity> listaEntidadesSP = GetListLoadedEntity(listadoIdentificadores, graph);
+            foreach (KeyValuePair<string, Entity> keyValue in listaEntidadesSP)
+            {
+                CvnItemBean itemBean = new CvnItemBean();
+                itemBean.Code = "060.010.030.000";
+                if (itemBean.Items == null)
+                {
+                    itemBean.Items = new List<CVNObject>();
+                }
+            }
 
+            //Añado en el cvnRootResultBean los items que forman parte del listado
+            UtilityExportar.AniadirItems(mCvn, listado);
         }
     }
 }
