9ae326368877cd84a79ea3c823c2111d37d6d892
Mario Heras <marioheras@gnoss.com>
2022-05-12 09:48:43 +0200

    Update PropiedadIndustrialIntelectual.cs

--- src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/ExperienciaCientificaSubclases/PropiedadIndustrialIntelectual.cs
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/ExperienciaCientificaSubclases/PropiedadIndustrialIntelectual.cs
@@ -1,16 +1,39 @@
-﻿using ImportadorWebCV;
+﻿using ExportadorWebCV.Utils;
+using ImportadorWebCV;
+using Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace ImportadorWebCV.Exporta.Secciones.ExperienciaCientificaSubclases
 {
     public class PropiedadIndustrialIntelectual:SeccionBase
     {
+        List<string> propiedadesItem = new List<string>() { "http://w3id.org/roh/scientificExperience", 
+            "http://w3id.org/roh/patents", "http://vivoweb.org/ontology/core#relatedBy" };
+        string graph = "patent";
         public PropiedadIndustrialIntelectual(cvnRootResultBean cvn, string cvID) : base(cvn, cvID)
         {
+        }
+        public void ExportaPropiedadII(Entity entity, string seccion, [Optional] List<string> secciones, [Optional] bool preimportar)
+        {
+            List<CvnItemBean> listado = new List<CvnItemBean>();
+            List<string> listadoIdentificadores = UtilityExportar.GetListadoEntidades(mResourceApi, propiedadesItem, mCvID);
+            Dictionary<string, Entity> listaEntidadesSP = GetListLoadedEntity(listadoIdentificadores, graph);
+            foreach (KeyValuePair<string, Entity> keyValue in listaEntidadesSP)
+            {
+                CvnItemBean itemBean = new CvnItemBean();
+                itemBean.Code = "050.030.010.000";
+                if (itemBean.Items == null)
+                {
+                    itemBean.Items = new List<CVNObject>();
+                }
+            }
 
+            //Añado en el cvnRootResultBean los items que forman parte del listado
+            UtilityExportar.AniadirItems(mCvn, listado);
         }
     }
 }
