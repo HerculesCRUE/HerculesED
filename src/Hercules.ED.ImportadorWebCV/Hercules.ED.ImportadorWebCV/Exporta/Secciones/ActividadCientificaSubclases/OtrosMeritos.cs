2c1841577b9742361539abb1424e89b636f3c26a
Mario Heras <marioheras@gnoss.com>
2022-05-12 15:25:12 +0200

    Update OtrosMeritos.cs

--- src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/ActividadCientificaSubclases/OtrosMeritos.cs
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/ActividadCientificaSubclases/OtrosMeritos.cs
@@ -16,8 +16,14 @@ namespace ImportadorWebCV.Exporta.Secciones.ActividadCientificaSubclases
         string graph = "accreditation";
         public OtrosMeritos(cvnRootResultBean cvn, string cvID) : base(cvn, cvID)
         {
-
         }
+        /// <summary>
+        /// Exporta los datos de la sección "060.030.100.000" a cvn.cvnRootResultBean
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="seccion"></param>
+        /// <param name="secciones"></param>
+        /// <param name="preimportar"></param>
         public void ExportaOtrosMeritos(Entity entity, string seccion, [Optional] List<string> secciones, [Optional] bool preimportar)
         {
             List<CvnItemBean> listado = new List<CvnItemBean>();
