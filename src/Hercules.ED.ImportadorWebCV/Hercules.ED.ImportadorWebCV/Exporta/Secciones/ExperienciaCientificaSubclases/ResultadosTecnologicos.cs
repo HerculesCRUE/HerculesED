435ab33bd8933870868e72e80f18f22d3870eace
Mario Heras <marioheras@gnoss.com>
2022-05-12 11:17:44 +0200

    Update ResultadosTecnologicos.cs

--- src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/ExperienciaCientificaSubclases/ResultadosTecnologicos.cs
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/ExperienciaCientificaSubclases/ResultadosTecnologicos.cs
@@ -30,6 +30,36 @@ namespace ImportadorWebCV.Exporta.Secciones.ExperienciaCientificaSubclases
                     Items = new List<CVNObject>()
                 };
 
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.resultadosTecnologicosDescripcion),
+                    "050.030.020.010", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.resultadosTecnologicosGradoContribucion),
+                    "050.030.020.070", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.resultadosTecnologicosGradoContribucionOtros),
+                    "050.030.020.080", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnBoolean(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.resultadosTecnologicosNuevasTecnicasEquip),
+                    "050.030.020.090", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnBoolean(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.resultadosTecnologicosEmpresasSpinOff),
+                    "050.030.020.100", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnBoolean(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.resultadosTecnologicosResultadosMejoraProd),
+                    "050.030.020.110", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnBoolean(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.resultadosTecnologicosHomologos),
+                    "050.030.020.120", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnBoolean(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.resultadosTecnologicosExpertoTecnologico),
+                    "050.030.020.130", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnBoolean(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.resultadosTecnologicosConveniosColab),
+                    "050.030.020.140", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.resultadosTecnologicosAmbitoActividad),
+                    "050.030.020.150", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.resultadosTecnologicosAmbitoActividadOtros),
+                    "050.030.020.160", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnDateDayMonthYear(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.resultadosTecnologicosFechaInicio),
+                    "050.030.020.250", keyValue.Value);
+
+                UtilityExportar.AddCvnItemBeanCvnDuration(itemBean, "050.030.020.260", keyValue.Value);
+
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.resultadosTecnologicosResultadosRelevantes),
+                    "050.030.020.270", keyValue.Value);
+
             }
 
             //Añado en el cvnRootResultBean los items que forman parte del listado
