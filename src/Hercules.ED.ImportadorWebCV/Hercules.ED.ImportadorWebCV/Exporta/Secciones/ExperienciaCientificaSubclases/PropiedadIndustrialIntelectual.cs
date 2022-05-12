f8672c627f66ce3905accc28be232cb89bcf2aa6
Mario Heras <marioheras@gnoss.com>
2022-05-12 09:49:19 +0200

    Update PropiedadIndustrialIntelectual.cs

--- src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/ExperienciaCientificaSubclases/PropiedadIndustrialIntelectual.cs
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/ExperienciaCientificaSubclases/PropiedadIndustrialIntelectual.cs
@@ -30,6 +30,80 @@ namespace ImportadorWebCV.Exporta.Secciones.ExperienciaCientificaSubclases
                 {
                     itemBean.Items = new List<CVNObject>();
                 }
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIIDescripcion),
+                    "050.030.010.010", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIITituloPropIndus),
+                    "050.030.010.020", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIITipoPropIndus),
+                    "050.030.010.030", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIITipoPropIndusOtros),
+                    "050.030.010.040", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnBoolean(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIIDerechosAutor),
+                    "050.030.010.050", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnBoolean(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIIDerechosConexos),
+                    "050.030.010.060", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnBoolean(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIISecretoEmpresarial),
+                    "050.030.010.070", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnBoolean(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIIModalidadKnowHow),
+                    "050.030.010.080", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnExternalPKBean(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIICodReferencia),
+                    "050.030.010.100", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnExternalPKBean(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIINumSolicitud),
+                    "050.030.010.110", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIIPaisInscripcion),
+                    "050.030.010.120", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIICCAAInscripcion),
+                    "050.030.010.130", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnDateDayMonthYear(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIIFechaRegistro),
+                    "050.030.010.150", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnBoolean(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIIPatenteEsp),
+                    "050.030.010.160", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnBoolean(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIIPatenteUE),
+                    "050.030.010.170", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnBoolean(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIIPatenteNoUE),
+                    "050.030.010.180", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIIResultadosRelevantes),
+                    "050.030.010.190", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnBoolean(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIILicencias),
+                    "050.030.010.210", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnBoolean(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIIExplotacionExclusiva),
+                    "050.030.010.260", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnBoolean(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIIGeneradaEmpresaInnov),
+                    "050.030.010.270", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIIResultadoEmpresaInnov),
+                    "050.030.010.280", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnExternalPKBean(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIINumPatente),
+                    "050.030.010.310", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnDateDayMonthYear(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIIFechaConcesion),
+                    "050.030.010.320", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnBoolean(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIIPatentePCT),
+                    "050.030.010.330", keyValue.Value);
+
+                // Entidad titular derechos
+                UtilityExportar.AddCvnItemBeanCvnEntityBean(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIIEntidadTitularDerechosNombre),
+                    "050.030.010.300", keyValue.Value);
+
+                // Empresas repeticiones
+                UtilityExportar.AddCvnItemBeanCvnEntityBeanList(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIIEmpresasExplotacionNombre),
+                    "050.030.010.250", keyValue.Value);
+
+                // Productos repeticiones
+                UtilityExportar.AddCvnItemBeanCvnStringList(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIINombreProductos),
+                    "050.030.010.290", keyValue.Value);
+
+                // Autores TODO
+
+                // Pais explotacion Codegroup repeticiones
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIIPaisExplotacion),
+                    "050.030.010.220", keyValue.Value);
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIICCAAExplotacion),
+                    "050.030.010.230", keyValue.Value);
+
+                // Palabras clave
+                UtilityExportar.AddCvnItemBeanCvnKeyword(itemBean, UtilityExportar.EliminarRDF(Variables.ExperienciaCientificaTecnologica.propIIPalabrasClave),
+                    "050.030.010.200", keyValue.Value);
+
+                listado.Add(itemBean);
             }
 
             //Añado en el cvnRootResultBean los items que forman parte del listado
