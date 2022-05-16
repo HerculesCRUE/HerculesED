9f56b70909628b3ec6aeae748be62f48e44b5a3d
Adrián <88077103+AdrianSaavedra-GNOSS@users.noreply.github.com>
2022-05-16 09:14:48 +0200

    Update OAI-PMH
    - Agregado Invenciones.

---
+++ src/Hercules.ED.OAI_PMH/OAI_PMH/Models/SGI/ProteccionIndustrialIntelectual/AreaConocimiento.cs
@@ -0,0 +1,9 @@
+﻿namespace OAI_PMH.Models.SGI.ProteccionIndustrialIntelectual
+{
+    public class AreaConocimiento
+    {
+        public int id { get; set; }
+        public int invencionId { get; set; }
+        public string areaConocimientoRef { get; set; }
+    }
+}
---
+++ src/Hercules.ED.OAI_PMH/OAI_PMH/Models/SGI/ProteccionIndustrialIntelectual/Titular.cs
@@ -0,0 +1,10 @@
+﻿namespace OAI_PMH.Models.SGI.ProteccionIndustrialIntelectual
+{
+    public class Titular
+    {
+        public int id { get; set; }
+        public int periodoTitularidadId { get; set; }
+        public string titularRef { get; set; }
+        public float participacion { get; set; }
+    }
+}
