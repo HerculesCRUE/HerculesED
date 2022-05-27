9c322e8df830895010ad6ba0a9d36f829288b9f0
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2022-05-27 08:16:01 +0200

    Update AccionesGuardado.cs

--- src/Hercules.ED.EditorCV/EditorCV/Models/AccionesGuardado.cs
+++ src/Hercules.ED.EditorCV/EditorCV/Models/AccionesGuardado.cs
@@ -31,7 +31,7 @@ namespace EditorCV.Models
         /// <summary>
         /// API
         /// </summary>
-        private static readonly ResourceApi mResourceApi = new ResourceApi($@"{System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase}Config/configOAuth/OAuthV3.config");
+        private static readonly ResourceApi mResourceApi = new ResourceApi($@"{System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase}Config/ConfigOAuth/OAuthV3.config");
 
         /// <summary>
         /// Cambia la privacidad de un item
