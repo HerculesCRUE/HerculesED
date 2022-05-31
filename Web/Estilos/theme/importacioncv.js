ef3e08de4a73202b73022bf0cf5ce05d3e397834
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2022-05-31 11:54:50 +0200

    Update importacioncv.js

--- Web/Estilos/theme/importacioncv.js
+++ Web/Estilos/theme/importacioncv.js
@@ -27,13 +27,14 @@ var importarCVN = {
     cargarCV: function() {
 		$('.col-contenido.paso1').hide();
 		$('.col-contenido.paso2').show();
-		var data = new FormData();
-		//data.append('pCVID', 'idcv');
-		data.append('File', $('#file_cvn')[0].files[0]);
+		var formData = new FormData();
+		formData.append('pCVID', 'idcv');
+		formData.append('File', $('#file_cvn')[0].files[0]);
 		 
 		$.ajax({
 			url: 'http://serviciosedma.gnoss.com/importadorcv/ImportadorCV/Preimportar',
-			data: data,
+			headers: { 'Content-Type': 'multipart/form-data;' },
+			data: formData,
 			processData: false,
 			type: 'POST',
 			success: function ( data ) {
