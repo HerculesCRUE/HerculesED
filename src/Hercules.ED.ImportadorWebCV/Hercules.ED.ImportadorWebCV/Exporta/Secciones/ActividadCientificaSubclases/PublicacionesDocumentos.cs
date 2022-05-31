f510d3ff58d1e65a5eda19d6cb41d3b4873da2e7
ManuelGNOSS <manuelboillos@gnoss.com>
2022-05-31 13:12:20 +0200

    Update PublicacionesDocumentos.cs

--- src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/ActividadCientificaSubclases/PublicacionesDocumentos.cs
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Exporta/Secciones/ActividadCientificaSubclases/PublicacionesDocumentos.cs
@@ -102,11 +102,11 @@ namespace ImportadorWebCV.Exporta.Secciones.ActividadCientificaSubclases
                     "060.010.010.390", keyValue.Value);
 
                 // Soporte
-                UtilityExportar.AddCvnItemBeanCvnStringTipoSoporte(itemBean, UtilityExportar.EliminarRDF(Variables.ActividadCientificaTecnologica.pubDocumentosTipoSoporte),
+                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ActividadCientificaTecnologica.pubDocumentosTipoSoporte),
                     "060.010.010.070", keyValue.Value);
                 UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ActividadCientificaTecnologica.pubDocumentosPubEditorial),
                     "060.010.010.100", keyValue.Value);
-                UtilityExportar.AddCvnItemBeanCvnString(itemBean, UtilityExportar.EliminarRDF(Variables.ActividadCientificaTecnologica.pubDocumentosPubNombre),
+                UtilityExportar.AddCvnItemBeanCvnStringTipoSoporte(itemBean, UtilityExportar.EliminarRDF(Variables.ActividadCientificaTecnologica.pubDocumentosPubNombre),
                     "060.010.010.210", keyValue.Value);
 
                 // Autores 
