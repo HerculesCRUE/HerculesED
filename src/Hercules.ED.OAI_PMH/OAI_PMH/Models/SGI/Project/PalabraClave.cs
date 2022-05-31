a1ab6008df186501eecbc1c49e3f40c16bd59ff9
Adrián <88077103+AdrianSaavedra-GNOSS@users.noreply.github.com>
2022-05-31 11:34:42 +0200

    Update OAI-PMH
    - Revisados Proyectos y PII

---
+++ src/Hercules.ED.OAI_PMH/OAI_PMH/Models/SGI/Project/PalabraClave.cs
@@ -0,0 +1,9 @@
+﻿namespace OAI_PMH.Models.SGI.Project
+{
+    public class PalabraClave : SGI_Base
+    {
+        public int id { get; set; }
+        public int proyectoId { get; set; }
+        public string palabraClaveRef { get; set; }
+    }
+}
