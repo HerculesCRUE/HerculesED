811001571ee3ada67e52355e6fa60708b7a60bcf
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2022-06-02 16:18:00 +0200

    Eliminado de los proyectos las propiedades de desambiguación de fecha y entidad de realización

--- src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Sincro/Secciones/ExperienciaCientificaSubclases/Contratos.cs
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Sincro/Secciones/ExperienciaCientificaSubclases/Contratos.cs
@@ -11,26 +11,11 @@ namespace ImportadorWebCV.Sincro.Secciones.ExperienciaCientificaSubclases
     class Contratos : DisambiguableEntity
     {
         public string nombre { get; set; }
-        public string fecha { get; set; }
-        public string entidadRealizacion { get; set; }
 
         private static readonly DisambiguationDataConfig configNombre = new DisambiguationDataConfig()
         {
             type = DisambiguationDataConfigType.equalsTitle,
-            score = 0.8f
-        };
-
-        private static readonly DisambiguationDataConfig configFecha = new DisambiguationDataConfig()
-        {
-            type = DisambiguationDataConfigType.equalsItem,
-            score = 0.5f
-        };
-        
-        private static readonly DisambiguationDataConfig configER = new DisambiguationDataConfig()
-        {
-            type = DisambiguationDataConfigType.equalsItem,
-            score = 0.5f,
-            scoreMinus = 0.5f
+            score = 1f
         };
 
         public override List<DisambiguationData> GetDisambiguationData()
@@ -42,20 +27,6 @@ namespace ImportadorWebCV.Sincro.Secciones.ExperienciaCientificaSubclases
                     property = "nombre",
                     config = configNombre,
                     value = nombre
-                },
-
-                new DisambiguationData()
-                {
-                    property = "fecha",
-                    config = configFecha,
-                    value = fecha
-                },
-
-                new DisambiguationData()
-                {
-                    property = "entidadRealizacion",
-                    config = configER,
-                    value = entidadRealizacion
                 }
             };
             return data;
@@ -73,11 +44,9 @@ namespace ImportadorWebCV.Sincro.Secciones.ExperienciaCientificaSubclases
 
             foreach (List<string> lista in listaListas)
             {
-                string select = $@"SELECT distinct ?item ?itemTitle ?itemDate ?itemER ";
+                string select = $@"SELECT distinct ?item ?itemTitle ";
                 string where = $@"where {{
                                         ?item <{Variables.ExperienciaCientificaTecnologica.contratosNombreProyecto}> ?itemTitle . 
-                                        OPTIONAL{{?item <{Variables.ExperienciaCientificaTecnologica.contratosFechaInicio}> ?itemDate }}.
-                                        OPTIONAL{{?item <{Variables.ExperienciaCientificaTecnologica.contratosEntidadRealizacionNombre}> ?itemER }}.
                                         FILTER(?item in (<{string.Join(">,<", lista)}>))
                                     }}";
                 SparqlObject resultData = pResourceApi.VirtuosoQuery(select, where, graph);
@@ -87,8 +56,6 @@ namespace ImportadorWebCV.Sincro.Secciones.ExperienciaCientificaSubclases
                     {
                         ID = fila["item"].value,
                         nombre = fila["itemTitle"].value,
-                        fecha = fila.ContainsKey("itemDate") ? fila["itemDate"].value : "",
-                        entidadRealizacion = fila.ContainsKey("itemER") ? fila["itemER"].value : ""
                     };
 
                     resultados.Add(pResourceApi.GetShortGuid(fila["item"].value).ToString(), contratos);
--- src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Sincro/Secciones/ExperienciaCientificaSubclases/ProyectosIDI.cs
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Sincro/Secciones/ExperienciaCientificaSubclases/ProyectosIDI.cs
@@ -11,26 +11,11 @@ namespace ImportadorWebCV.Sincro.Secciones.ExperienciaCientificaSubclases
     class ProyectosIDI : DisambiguableEntity
     {
         public string nombre { get; set; }
-        public string fecha { get; set; }
-        public string entidadRealizacion { get; set; }
 
         private static readonly DisambiguationDataConfig configNombre = new DisambiguationDataConfig()
         {
             type = DisambiguationDataConfigType.equalsTitle,
-            score = 0.8f
-        };
-
-        private static readonly DisambiguationDataConfig configFecha = new DisambiguationDataConfig()
-        {
-            type = DisambiguationDataConfigType.equalsItem,
-            score = 0.5f
-        };
-        
-        private static readonly DisambiguationDataConfig configER = new DisambiguationDataConfig()
-        {
-            type = DisambiguationDataConfigType.equalsItem,
-            score = 0.5f,
-            scoreMinus = 0.5f
+            score = 1f
         };
 
         public override List<DisambiguationData> GetDisambiguationData()
@@ -43,20 +28,6 @@ namespace ImportadorWebCV.Sincro.Secciones.ExperienciaCientificaSubclases
                     config = configNombre,
                     value = nombre
                 },
-
-                new DisambiguationData()
-                {
-                    property = "fecha",
-                    config = configFecha,
-                    value = fecha
-                },
-
-                new DisambiguationData()
-                {
-                    property = "entidadRealizacion",
-                    config = configER,
-                    value = entidadRealizacion
-                }
             };
             return data;
         }
@@ -73,11 +44,9 @@ namespace ImportadorWebCV.Sincro.Secciones.ExperienciaCientificaSubclases
 
             foreach (List<string> lista in listaListas)
             {
-                string select = $@"SELECT distinct ?item ?itemTitle ?itemDate ?itemER ";
+                string select = $@"SELECT distinct ?item ?itemTitle ";
                 string where = $@"where {{
                                         ?item <{Variables.ExperienciaCientificaTecnologica.proyectosIDINombre}> ?itemTitle . 
-                                        OPTIONAL{{?item <{Variables.ExperienciaCientificaTecnologica.proyectosIDIFechaInicio}> ?itemDate }}.
-                                        OPTIONAL{{?item <{Variables.ExperienciaCientificaTecnologica.proyectosIDIEntidadRealizacionNombre}> ?itemER }}.
                                         FILTER(?item in (<{string.Join(">,<", lista)}>))
                                     }}";
                 SparqlObject resultData = pResourceApi.VirtuosoQuery(select, where, graph);
@@ -86,9 +55,7 @@ namespace ImportadorWebCV.Sincro.Secciones.ExperienciaCientificaSubclases
                     ProyectosIDI proyectosIDI = new ProyectosIDI
                     {
                         ID = fila["item"].value,
-                        nombre = fila["itemTitle"].value,
-                        fecha = fila.ContainsKey("itemDate") ? fila["itemDate"].value : "",
-                        entidadRealizacion = fila.ContainsKey("itemER") ? fila["itemER"].value : ""
+                        nombre = fila["itemTitle"].value
                     };
 
                     resultados.Add(pResourceApi.GetShortGuid(fila["item"].value).ToString(), proyectosIDI);
