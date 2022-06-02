2776a9325a9ee97c7cd9549874791aa07b17ec66
ManuelGNOSS <manuelboillos@gnoss.com>
2022-06-02 08:10:56 +0200

    Revert "Cambios double"
    This reverts commit badbaef558d3cdd5e66ed06609366fcd3a53d7fc.

--- src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/CVN.cs
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/CVN.cs
@@ -240,7 +240,7 @@ namespace ImportadorWebCV
     {
 
         /// <remarks/>
-        public string Value { get; set; }
+        public decimal Value { get; set; }
     }
 
     /// <remarks/>
--- src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Utils/UtilityExportar.cs
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Utils/UtilityExportar.cs
@@ -1144,11 +1144,11 @@ namespace Utils
         {
             CvnItemBeanCvnDouble cvnDouble = new CvnItemBeanCvnDouble();
             cvnDouble.Code = code;
-            if (value.Contains(","))
+            if (value.Contains("."))
             {
-                value = value.Replace(",", ".");
+                value = value.Replace(".", ",");
             }
-            cvnDouble.Value = value;
+            cvnDouble.Value = Convert.ToDecimal(value);
 
             itemBean.Items.Add(cvnDouble);
         }
