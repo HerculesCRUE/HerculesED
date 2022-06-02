99591fbf5df20e6a3891ccd7f6e778ebf81b2da8
ManuelGNOSS <manuelboillos@gnoss.com>
2022-06-02 11:15:14 +0200

    Revert "Update ExportadorCVController.cs"
    This reverts commit 2bbe4272a1bfbe24c97c1f9b1a72391079483534.

--- src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Controllers/ExportadorCVController.cs
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/Controllers/ExportadorCVController.cs
@@ -124,7 +124,6 @@ namespace Hercules.ED.ExportadorWebCV.Controllers
             var resp = peticion.Result.@return;
             client.Close();
 
-            System.Globalization.CultureInfo.CurrentCulture = System.Globalization.CultureInfo.GetCultureInfo("es-ES");
             return File(resp.dataHandler, "application/pdf");
         }
     }
