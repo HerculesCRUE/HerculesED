28f2c4a6963cdd3f6ef8b68154d19d14a6a7a4d6
Adrián <88077103+AdrianSaavedra-GNOSS@users.noreply.github.com>
2022-06-14 16:28:12 +0200

    Update GraphicEngine

--- src/Hercules.ED.GraphicEngine/Hercules.ED.GraphicEngine/Models/Paginas/Pagina.cs
+++ src/Hercules.ED.GraphicEngine/Hercules.ED.GraphicEngine/Models/Paginas/Pagina.cs
@@ -14,5 +14,11 @@ namespace Hercules.ED.GraphicEngine.Models.Paginas
         public string id { get; set; }
         public int anchura { get; set; }
         public string idGrupo { get; set; }
+        public bool isAbr { get; set; }
+        public bool isPercentage { get; set; }
+        public bool isHorizontal { get; set; }
+        public bool isVertical { get; set; }
+        public bool isNodes { get; set; }
+        public bool isCircular { get; set; }
     }
 }
