6d8702170c0d934b9dd7f5b50df1dff5d981ccf0
ManuelGNOSS <manuelboillos@gnoss.com>
2022-06-16 08:11:03 +0200

    Update AccionesImportacion.cs

--- src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/AccionesImportacion.cs
+++ src/Hercules.ED.ImportadorWebCV/Hercules.ED.ImportadorWebCV/AccionesImportacion.cs
@@ -7,8 +7,10 @@ using ImportadorWebCV.Sincro;
 using Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Hercules.ED.ImportadorWebCV
 {
@@ -21,10 +23,22 @@ namespace Hercules.ED.ImportadorWebCV
 
         }
 
-        public void ImportacionTriples(string pCVID, List<string> listaId, List<string> listaOpciones)
+        public void ImportacionTriples(string pCVID, string filePreimport, List<string> listaId, List<string> listaOpciones)
         {
             List<Subseccion> listadoSubsecciones = new List<Subseccion>();
             Dictionary<string, string> dicOpciones = new Dictionary<string, string>();
+            Dictionary<string, string> filtrador = new Dictionary<string, string>();
+            foreach (string str in listaId)
+            {
+                filtrador.Add(str.Split("_").First(), str.Split("_").Last());
+            }
+            Preimport preimport = new Preimport();
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Preimport));
+            using (TextReader reader = new StringReader(filePreimport))
+            {
+                preimport = (Preimport)serializer.Deserialize(reader);
+            }
 
             string idOpcion;
             string valueOpcion;
@@ -36,102 +50,74 @@ namespace Hercules.ED.ImportadorWebCV
                     {
                         continue;
                     }
-                    idOpcion = opcion.Split("|||").First();
+                    idOpcion = opcion.Split("|||").First().Split("_").Last();
                     valueOpcion = opcion.Split("|||").Last();
                     dicOpciones.Add(idOpcion, valueOpcion);
                 }
             }
 
             List<CvnItemBean> listadoItems = base.cvn.cvnRootBean.ToList();
-            //foreach (Subseccion subseccion in sincroDatos.preimport.secciones)
-            //{
-            //    Subseccion subsec = new Subseccion(subseccion.id);
-            //    foreach (SubseccionItem subseccionItem in subseccion.subsecciones)
-            //    {
-            //        if (listaId.Contains(subseccionItem.guid))
-            //        {
-            //            subsec.subsecciones.Add(subseccionItem);
-            //        }
-            //    }
-            //    listadoSubsecciones.Add(subsec);
-            //}
-            //sincroDatos.preimport = new Preimport(listadoSubsecciones);
-            //List<TriplesToInclude> triplesToInclude = new List<TriplesToInclude>();
-            //foreach (Subseccion sub in sincroDatos.preimport.secciones)
-            //{
-            //    foreach (SubseccionItem subseccionItem in sub.subsecciones)
-            //    {
-            //        string idBBDD = subseccionItem.idBBDD;
-            //        bool isBlocked = subseccionItem.isBlocked;
+            if (listadoItems.Count == 0 || !listadoItems.ElementAt(0).Code.Equals("000.020.000.000"))
+            {
+                return;
+            }
 
-            //        if (!listaId.Contains(subseccionItem.guid))
-            //        {
-            //            continue;
-            //        }
+            string opcionSeleccionada = "";
+            List<CvnItemBean> listadoDuplicar = new List<CvnItemBean>();
+            List<CvnItemBean> listadoFusionar = new List<CvnItemBean>();
+            List<CvnItemBean> listadoSobrescribir = new List<CvnItemBean>();
+            List<string> listadoDuplicarBBDD = new List<string>();
+            List<string> listadoFusionarBBDD = new List<string>();
+            List<string> listadoSobrescribirBBDD = new List<string>();
+            for (int i = 1; i < listadoItems.Count; i++)
+            {
+                if (!filtrador.ContainsValue(i.ToString()))
+                {
+                    continue;
+                }
+                opcionSeleccionada = "so";
+                if (dicOpciones.ContainsKey(i.ToString()))
+                {
+                    opcionSeleccionada = dicOpciones[i.ToString()];
+                }
+                if (opcionSeleccionada.Equals("du"))
+                {
+                    listadoDuplicar.Add(listadoItems.ElementAt(i));
+                    listadoDuplicarBBDD.Add(filtrador.Where(x=>x.Value.Equals(i.ToString())).Select(x=>x.Key).FirstOrDefault() + "_du");
+                }
+                if (opcionSeleccionada.Equals("fu"))
+                {
+                    listadoFusionar.Add(listadoItems.ElementAt(i));
+                    listadoFusionarBBDD.Add(filtrador.Where(x => x.Value.Equals(i.ToString())).Select(x => x.Key).FirstOrDefault() + "_fu");
+                }
+                if (opcionSeleccionada.Equals("so"))
+                {
+                    listadoSobrescribir.Add(listadoItems.ElementAt(i));
+                    listadoSobrescribirBBDD.Add(filtrador.Where(x => x.Value.Equals(i.ToString())).Select(x => x.Key).FirstOrDefault() + "_so");
+                }
+            }
 
-            //        string opcion = "du";
-            //        if (dicOpciones.Keys.Contains(subseccionItem.guid))
-            //        {
-            //            opcion = dicOpciones[subseccionItem.guid];
-            //        }
-
-            //        //Compruebo el tipo de opcion para tratar
-            //        switch (opcion)
-            //        {
-            //            //Fusionar
-            //            case "fu":
-            //                ///<param name="pIdMainEntity">Identificador de la entidad a la que pertenece la entidad auxiliar</param>
-            //                /// <param name="pPropertyIDs">Propiedades que apuntan a la auxiliar</param>
-            //                /// <param name="pEntityIDs">Entidades que apuntan a la auxiliar</param>
-            //                /// <param name="pLoadedEntity">Entidad cargada en BBDD</param>
-            //                /// <param name="pUpdatedEntity">Nueva entidad</param>
-            //                //Utils.UtilityCV.UpdateEntityAux(pCVID, listapropiedadesauxiliar, listaentidadesauxiliar, entidadBBDD, nueva);
-
-            //                break;
-
-            //            //Sobrescribir
-            //            case "so":
-            //                if (isBlocked)
-            //                {
-            //                    break;
-            //                }
-
-            //                break;
-
-            //            //Duplicar
-            //            case "du":
-
-            //                break;
-
-            //            default:
-            //                break;
-            //        }
+            cvnRootResultBean duplicadosResultBean = new cvnRootResultBean() { cvnRootBean = listadoDuplicar.ToArray() };
+            cvnRootResultBean fusionResultBean = new cvnRootResultBean() { cvnRootBean = listadoFusionar.ToArray() };
+            cvnRootResultBean sobrescribirResultBean = new cvnRootResultBean() { cvnRootBean = listadoSobrescribir.ToArray() };
 
 
-            //        //añado en triplestoinclude/triplestomodify/triplestodelete las propiedades 
-            //        //inserto/modifico el recurso                        
+            //Duplicar
+            base.cvn = duplicadosResultBean;
+            base.SincroDatosIdentificacion(preimportar: false);
+            base.SincroDatosSituacionProfesional(preimportar: false, listadoIdBBDD: listadoDuplicarBBDD);
+            base.SincroFormacionAcademica(preimportar: false, listadoIdBBDD: listadoDuplicarBBDD);
+            base.SincroActividadDocente(preimportar: false, listadoIdBBDD: listadoDuplicarBBDD);
+            base.SincroExperienciaCientificaTecnologica(preimportar: false, listadoIdBBDD: listadoDuplicarBBDD);
+            base.SincroActividadCientificaTecnologica(preimportar: false, listadoIdBBDD: listadoDuplicarBBDD);
+            base.SincroTextoLibre(preimportar: false);
 
-            //        foreach (Entity.Property property in subseccionItem.propiedades)
-            //        {
-            //            foreach (string value in property.values)
-            //            {
-            //                triplesToInclude.Add(new TriplesToInclude(property.prop, value));
-            //            }
-            //        }
+            //Fusionar - TODO
+            base.cvn = fusionResultBean;
 
-
-            //        {
-            //            Dictionary<Guid, List<TriplesToInclude>> triplesInclude = new Dictionary<Guid, List<TriplesToInclude>>() { { mResourceApi.GetShortGuid(pCVID), triplesToInclude } };
-            //            bool b = mResourceApi.InsertPropertiesLoadedResources(triplesInclude)[mResourceApi.GetShortGuid(subseccionItem.idBBDD)];
-
-
-
-            //            Utils.UtilityCV.UpdateEntityAux(Guid.NewGuid(), new List<string>(), new List<string>(), new Entity(), new Entity(), mResourceApi);
-            //        }
-            //    }
-            //}
-
-
+            //Sobrescribir - TODO
+            base.cvn = sobrescribirResultBean;
         }
     }
 }
+
