947724e9ffaf44818c2a7fa9b10fbb490f9653ad
ManuelGNOSS <manuelboillos@gnoss.com>
2022-06-21 16:21:41 +0200

    Update edicioncv.js

--- Web/Estilos/theme/edicioncv.js
+++ Web/Estilos/theme/edicioncv.js
@@ -3643,59 +3643,11 @@ var edicionCV = {
 		$('.texto.prodCientItem').off('click').on('click', function(e) {
 			$('#modal-enviar-produccion-cientifica .formulario-edicion.formulario-proyecto .resource-list-wrap').empty();
 			$('#modal-enviar-produccion-cientifica .resource-list.listView .resource-list-wrap').empty();
-			var html = `<article class="resource">
-                            <div class="wrap">
-                                <div class="middle-wrap">
-                                    <div class="title-wrap">
-                                        <h2 class="resource-title">
-                                            test
-                                        </h2>
-                                        <div class="block-wrapper" data-original-title="" title="">
-                                            <span class="material-icons">block</span>
-                                        </div>
-                                        <div class="visibility-wrapper">
-                                            <div class="con-icono-before eye" data-original-title="" title=""></div>
-                                        </div>
-                                        <span class="material-icons arrow">keyboard_arrow_down</span>
-                                    </div>
-                                    <div class="content-wrap">
-                                        <div class="description-wrap counted">
-                                            <div class="group fecha">
-                                                <p class="title">Fecha de publicación</p>
-                                                <p>03/01/2020</p>
-                                            </div>
-                                            <div class="group publicacion">
-                                                <p class="title">Nombre de la publicación</p>
-                                                <p>International Journal of Human-Computer Studies</p>
-                                            </div>
-                                            <div class="group autores">
-                                                <p class="title">Autores/as (p. o. de firma)</p>
-                                                <div class="list-wrap authors">
-                                                    <ul>
-                                                        <li>
-                                                            <a href="javascript: void(0);">International Journal of Human-Computer Studies</a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                            <div class="group identificador">
-                                                <p class="title">Identificador publicación</p>
-                                                <p>https://doi.org/10.1016/j.ijhcs.2019.102393</p>
-                                            </div>
-                                            <div class="group tipo-produccion">
-                                                <p class="title">Tipo de producción</p>
-                                                <p>Artículo científico</p>
-                                            </div>
-                                            <div class="group tipo-soporte">
-                                                <p class="title">Tipo de soporte</p>
-                                                <p>Revista</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </article>`;
-			$('#modal-enviar-produccion-cientifica div.modal-body>.resource-list.listView .resource-list-wrap').append(html);
+			
+			
+			$(this).closest("article").clone().appendTo('#modal-enviar-produccion-cientifica div.modal-body>.resource-list.listView .resource-list-wrap');
+			$('#modal-enviar-produccion-cientifica div.modal-body>.resource-list.listView .resource-list-wrap article .acciones-recurso-listado.acciones-recurso').remove();
+			$('#modal-enviar-produccion-cientifica div.modal-body>.resource-list.listView .resource-list-wrap article .material-icons.arrow').remove();
 			
 			
 			var dataId = $(this)[0].dataset.id;
