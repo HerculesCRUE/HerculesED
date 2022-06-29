9d3b11a33853c6e4df96ebfc04c3f90fd688c204
Mario Heras <marioheras@gnoss.com>
2022-06-29 08:15:53 +0200

    filtrado reciproco

--- Web/Estilos/theme/graphic-engine.js
+++ Web/Estilos/theme/graphic-engine.js
@@ -409,7 +409,7 @@ var metricas = {
                     $('div[idfaceta="' + data.id + '"] .listadoFacetas').append(`
                         <li>
                             <a href="javascript: void(0);" class="faceta filtroMetrica" filtro="${item.filtro}">
-                                <span class="textoFaceta">${item.nombre}</span>
+                                <span class="textoFaceta">${item.nombre == 'true' ? 'Sí' : (item.nombre == 'false' ? 'No' : item.nombre)}</span>
                                 <span class="num-resultados">(${item.numero})</span>
                             </a>
                         </li>
@@ -620,7 +620,7 @@ var metricas = {
         pPageData.listaIdsFacetas.forEach(function (item, index, array) {
             $('#page_' + pPageData.id + ' .containerFacetas').append(`
                     <div class='facetedSearch'>
-                        <div class='box' idfaceta="${item}"></div>
+                        <div class='box' idfaceta="${item.includes('(((') ? item.split('(((')[0] : item}" reciproca="${item.includes('(((') ? item.split('(((')[1] : ''}"></div>
                         </div>
                     `);
         });
@@ -801,10 +801,11 @@ var metricas = {
             }
             if (filtro.split('=')[1].includes('@')) {
                 nombre = filtro.split('=')[1].split('@')[0].replaceAll("'", "");
+                
                 $(".borrarFiltros-wrap").remove();
                 $("#panListadoFiltros").append(`
                 <li class="Categoria" filtro="${filtro}">
-                    <span>${nombre}</span>
+                    <span>${nombre == 'true' ? 'Sí' : (nombre == 'false' ? 'No' : (nombre.includes('(((') ? nombre.split('(((')[0] : nombre))}</span>
                     <a rel="nofollow" class="remove faceta" name="search=Categoria" href="javascript:;">eliminar</a>
                 </li>
                 <li class="borrarFiltros-wrap">
@@ -833,7 +834,7 @@ var metricas = {
                 $(".borrarFiltros-wrap").remove();
                 $("#panListadoFiltros").append(`
                 <li class="Categoria" filtro="${filtro}">
-                    <span>${nombre}</span>
+                    <span>${nombre == 'true' ? 'Sí' : (nombre == 'false' ? 'No' : (nombre.includes('(((') ? nombre.split('(((')[0] : nombre))}</span>
                     <a rel="nofollow" class="remove faceta" name="search=Categoria" href="javascript:;">eliminar</a>
                 </li>
                 <li class="borrarFiltros-wrap">
@@ -1295,7 +1296,7 @@ var metricas = {
                 return `<li>
                             <a rel="nofollow" href="javascript: void(0);" class="faceta filtroMetrica con-subfaceta ocultarSubFaceta ocultarSubFaceta" filtro="${pData.filtro}" title="${pData.nombre}">
                                 <span class="desplegarSubFaceta"><span class="material-icons">add</span></span>
-                                <span class="textoFaceta">${pData.nombre}</span>
+                                <span class="textoFaceta">${pData.nombre == 'true' ? 'Sí' : (pData.nombre == 'false' ? 'No' : pData.nombre)}</span>
                                 <span class="num-resultados">(${pData.numero})</span>                          
                             </a>
                             ${etiqueta}
@@ -1305,7 +1306,7 @@ var metricas = {
 
                 return `<li>
                             <a rel="nofollow" href="javascript: void(0);" class="faceta filtroMetrica ocultarSubFaceta ocultarSubFaceta" filtro="${pData.filtro}" title="${pData.nombre}">
-                                <span class="textoFaceta">${pData.nombre}</span>
+                                <span class="textoFaceta">${pData.nombre == 'true' ? 'Sí' : (pData.nombre == 'false' ? 'No' : pData.nombre)}</span>
                                 <span class="num-resultados">(${pData.numero})</span>                          
                             </a>
                         </li>`;
@@ -1352,6 +1353,9 @@ var metricas = {
             .unbind()
             .click(function (e) {
                 var filtroActual = $(this).attr('filtro');
+                if ($(this).parents('.box').attr('reciproca')) {
+                    filtroActual = filtroActual + '(((' + $(this).parents('.box').attr('reciproca');
+                }
                 var filtros = decodeURIComponent(ObtenerHash2());
                 var filtrosArray = filtros.split('&');
                 filtros = '';
@@ -2443,7 +2447,7 @@ comportamientoFacetasPopUp.cargarFaceta = function (pIdFaceta) {
             that.arrayTotales[i] = new Array(2);
             that.arrayTotales[i][0] = that.eliminarAcentos(item.nombre.toLowerCase());
             that.arrayTotales[i][1] = $(`<a href="javascript: void(0);" class="faceta filtroMetrica" filtro="${item.filtro}">
-                                <span class="textoFaceta">${item.nombre}</span>
+                                <span class="textoFaceta">${item.nombre == 'true' ? 'Sí' : (item.nombre == 'false' ? 'No' : item.nombre)}</span>
                                 <span class="num-resultados">(5)</span>
                             </a>`);
             i++;
