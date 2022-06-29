372ab98fb2558ff2db84b6a852dad7036a7b540e
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2022-06-29 12:14:03 +0200

    Create EnrichmentSimilarityPut.cs

---
+++ src/Hercules.ED.Desnormalizador/Models/Similarity/EnrichmentSimilarityPut.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesnormalizadorHercules.Models
+{
+    public class EnrichmentSimilarityPut
+    {
+        public string ro_id { get; set; }
+        public string ro_type { get; set; }
+        public string text { get; set; }
+        public List<string> authors { get; set; }
+        public List<List<object>> thematic_descriptors { get; set; }
+        public List<List<object>> specific_descriptors { get; set; }  
+    }
+
+}
