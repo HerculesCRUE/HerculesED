0af3f5baea8196f2e959114c96d8f1c8546a4976
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2022-06-29 12:14:05 +0200

    Create EnrichmentSimilarityPutResponse.cs

---
+++ src/Hercules.ED.Desnormalizador/Models/Similarity/EnrichmentSimilarityPutResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesnormalizadorHercules.Models.Similarity
+{
+    public class EnrichmentSimilarityPutResponse
+    {
+        public string error_msg { get; set; }
+    }
+
+}
