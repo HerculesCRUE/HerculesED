51e1508820d03777c79e21fe1387fe28ea5b8cdb
PabloDeGroot <pablodegroot@gnoss.com>
2022-07-06 08:45:40 +0200

    Update graphic-engine.js

--- Web/Estilos/theme/graphic-engine.js
+++ Web/Estilos/theme/graphic-engine.js
@@ -17,6 +17,7 @@ var ordenActual;
 var escalaActual;
 var TAMANIO_GRAFICA_MIN = 318;
 
+var facetas = false;
 var numPagina = 0;
 // Lista de páginas.
 var listaPaginas;
@@ -61,7 +62,7 @@ var metricas = {
         var url = url_servicio_graphicengine + "GetPaginasGraficas"; //"https://localhost:44352/GetPaginaGrafica";        
         var arg = {};
         arg.pLang = lang;
-
+        arg.userId = $('.inpt_usuarioID').attr('value');
         // Petición para obtener los datos de la página.
         $.get(url, arg, function (listaData) {
             for (let i = 0; i < listaData.length; i++) {
@@ -537,6 +538,7 @@ var metricas = {
         var url = url_servicio_graphicengine + "GetFaceta"; //"https://localhost:44352/GetFaceta"
         var arg = {};
 
+
         arg.pIdPagina = pIdPagina;
         arg.pIdFaceta = pIdFaceta;
         arg.pFiltroFacetas = pFiltroFacetas;
@@ -547,7 +549,13 @@ var metricas = {
         maxYear = 0;
 
         // Petición para obtener los datos de las gráficas.
+
         $.get(url, arg, function (data) {
+
+            if ($("div[idFaceta='" + pIdFaceta + "']").find("*").length > 0) {
+                return;
+             }
+
 
             var numItemsPintados = 0;
             if (data.isDate) {
@@ -827,15 +835,17 @@ var metricas = {
         });
 
 
-        // Crear estructura para el apartado de facetas.
-        pPageData.listaIdsFacetas.forEach(function (item, index, array) {
-            $('#page_' + pPageData.id + ' .containerFacetas').append(`
+        if ($("div.facetedSearch").length == 0) {
+            pPageData.listaIdsFacetas.forEach(function (item, index, array) {
+
+                    $('#page_' + pPageData.id + ' .containerFacetas').append(`
                     <div class='facetedSearch'>
                         <div class='box' idfaceta="${item.includes('(((') ? item.split('(((')[0] : item}" reciproca="${item.includes('(((') ? item.split('(((')[1] : ''}"></div>
                         </div>
                     `);
-        });
-
+  
+            });
+        }
         that.pintarPagina(pPageData.id)
     },
     fillPagePersonalized: function (pPaginaUsuario) {
