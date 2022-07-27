42371f62ed2d7edecc9e60da9d0c6679152a85d8
ManuelGNOSS <manuelboillos@gnoss.com>
2022-07-27 12:14:51 +0200

    Update ActividadCientificaTecnologica.cs

--- src/Hercules.ED.ImportExportCV/Hercules.ED.ImportExportCV/Sincro/Secciones/ActividadCientificaTecnologica.cs
+++ src/Hercules.ED.ImportExportCV/Hercules.ED.ImportExportCV/Sincro/Secciones/ActividadCientificaTecnologica.cs
@@ -96,6 +96,7 @@ namespace ImportadorWebCV.Sincro.Secciones
             {
                 return null;
             }
+
             //Actualizo el estado de los recursos tratados
             petitionStatus.actualWork++;
 
