9f1f472496cdd3e0b4bbc166fe82c723ed9ea2f2
ManuelGNOSS <manuelboillos@gnoss.com>
2022-07-27 10:32:10 +0200

    Update DatosIdentificacion.cs

--- src/Hercules.ED.ImportExportCV/Hercules.ED.ImportExportCV/Sincro/Secciones/DatosIdentificacion.cs
+++ src/Hercules.ED.ImportExportCV/Hercules.ED.ImportExportCV/Sincro/Secciones/DatosIdentificacion.cs
@@ -14,7 +14,7 @@ namespace ImportadorWebCV.Sincro.Secciones
 {
     public class DatosIdentificacion : SeccionBase
     {
-        public DatosIdentificacion(cvnRootResultBean cvn, string cvID,ConfigService configuracion) : base(cvn, cvID,configuracion)
+        public DatosIdentificacion(cvnRootResultBean cvn, string cvID, ConfigService configuracion) : base(cvn, cvID, configuracion)
         {
         }
 
@@ -30,9 +30,10 @@ namespace ImportadorWebCV.Sincro.Secciones
             //Actualizo el estado de los recursos tratados
             petitionStatus.actualWork++;
 
+
             //Si procesar es false, no hago nada.
             if (!procesar)
-            { 
+            {
                 return null;
             }
 
@@ -46,9 +47,9 @@ namespace ImportadorWebCV.Sincro.Secciones
             Entity entityXML = ObtenerDatosPersonales(entityBBDD, listadoDatosIdentificacion);
 
             if (preimportar)
-            {                
+            {
                 List<SubseccionItem> listaAux = new List<SubseccionItem>();
-                listaAux.Add( new SubseccionItem(0, entityBBDD.id, entityXML.properties));
+                listaAux.Add(new SubseccionItem(0, entityBBDD.id, entityXML.properties));
                 return listaAux;
             }
             else
@@ -115,11 +116,11 @@ namespace ImportadorWebCV.Sincro.Secciones
                     new Property(Variables.DatosIdentificacion.nie, listadoDatosIdentificacion.GetStringPorIDCampo("000.010.000.110")),
                     new Property(Variables.DatosIdentificacion.pasaporte, listadoDatosIdentificacion.GetStringPorIDCampo("000.010.000.120")),
                     new Property(Variables.DatosIdentificacion.imagenDigital, listadoDatosIdentificacion.GetImagenPorIDCampo("000.010.000.130")),
-                    new Property(Variables.DatosIdentificacion.email, listadoDatosIdentificacion.GetStringPorIDCampo("000.010.000.230")),                    
+                    new Property(Variables.DatosIdentificacion.email, listadoDatosIdentificacion.GetStringPorIDCampo("000.010.000.230")),
                     new Property(Variables.DatosIdentificacion.paginaWeb, listadoDatosIdentificacion.GetStringPorIDCampo("000.010.000.250")),
                     new Property(Variables.DatosIdentificacion.ORCID, listadoDatosIdentificacion.GetListaElementosPorIDCampo<CvnItemBeanCvnExternalPKBean>("000.010.000.260").GetORCID()),
                     new Property(Variables.DatosIdentificacion.scopus, listadoDatosIdentificacion.GetListaElementosPorIDCampo<CvnItemBeanCvnExternalPKBean>("000.010.000.260").GetScopus()),
-                    new Property(Variables.DatosIdentificacion.researcherId, listadoDatosIdentificacion.GetListaElementosPorIDCampo<CvnItemBeanCvnExternalPKBean>("000.010.000.260").GetResearcherID())                
+                    new Property(Variables.DatosIdentificacion.researcherId, listadoDatosIdentificacion.GetListaElementosPorIDCampo<CvnItemBeanCvnExternalPKBean>("000.010.000.260").GetResearcherID())
                 );
                 GetDireccionNacimiento(listadoDatosIdentificacion, entity, entityBBDD);
                 GetDireccionContacto(listadoDatosIdentificacion, entity, entityBBDD);
