513c6faa6b031d94c6a6c5c194019f230ffff334
Mario Heras <marioheras@gnoss.com>
2022-07-28 10:23:27 +0200

    Update edicioncv.js

--- Web/Estilos/theme/edicioncv.js
+++ Web/Estilos/theme/edicioncv.js
@@ -72,6 +72,7 @@ var edicionCV = {
         $.get(urlEdicionCV + 'GetTab?pCVId='+that.idCV+'&pId=' + entityID + "&pRdfType=" + rdfType + "&pLang=" + lang+ "&pSection=0", null, function(data) {
             that.printTab(entityID, data);
             OcultarUpdateProgress();
+			$('a#actividad-tab.nav-link.active').tooltip('hide');
 			for(var key in tooltips.section) {
 				var value = tooltips.section[key];
 				$(key).tooltip({
