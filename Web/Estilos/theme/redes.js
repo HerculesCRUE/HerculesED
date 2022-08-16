ca2163b37db49bf453824b07400cd8dcb52840df
Mario Heras <marioheras@gnoss.com>
2022-08-16 12:02:12 +0200

    Update redes.js

--- Web/Estilos/theme/redes.js
+++ Web/Estilos/theme/redes.js
@@ -31,7 +31,11 @@ $(document).ready(function () {
             } else if (valor.id == "useMatching") {
                 var html = `<div class="form-group mb-4"><div class="d-flex matchingTooltip"><label id="${valor.id}" class="control-label d-block">${label}</label><span class="material-icons-outlined" style="margin-left: 10px; width: 24px"">information</span></div><div class="form-check form-check-inline" style="flex-flow: nowrap column"><div style="width:100%"><input type="radio" name="radioMatching" id="check-si" value="true" class="form-check-input form-control not-outline"> <label for="html">Sí</label></div> <div style="width:100%"><input type="radio" name="radioMatching" id="check-no" value="false" class="form-check-input form-control not-outline"> <label for="html">No</label></div> </div></div>`;
             } else if (valor.id == "orcid" || valor.id == "researcherId" || valor.id == "scopusId" || valor.id == "semanticScholarId") {
-                var html = `<div class="form-group mb-4"><label id="${valor.id}" class="control-label d-block">${label}</label> <input disabled placeholder="" type="text" name="fname" id="" value="${valor.valor}" class="form-control not-outline"></div>`;
+                if ($('div.identificador').length % 2 == 0) {
+                    var html = `<div class="form-group mb-4 identificador" style="width: 50%; float:left; padding-right:3%"><label id="${valor.id}" class="control-label d-block">${label}</label> <input disabled placeholder="" type="text" name="fname" id="" value="${valor.valor}" class="form-control not-outline"></div>`;
+                } else {
+                    var html = `<div class="form-group mb-4 identificador" style="width: 50%; float:left; padding-left:3%"><label id="${valor.id}" class="control-label d-block">${label}</label> <input disabled placeholder="" type="text" name="fname" id="" value="${valor.valor}" class="form-control not-outline"></div>`;
+                }
             }
 
             $('form.formulario-edicion fieldset').append(html);
