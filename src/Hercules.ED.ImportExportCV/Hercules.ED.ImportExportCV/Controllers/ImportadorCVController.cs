314d85a9205972ba8ff35378f696cafe34dfc622
ManuelGNOSS <manuelboillos@gnoss.com>
2022-08-19 12:21:15 +0200

    Update ImportadorCVController.cs

--- src/Hercules.ED.ImportExportCV/Hercules.ED.ImportExportCV/Controllers/ImportadorCVController.cs
+++ src/Hercules.ED.ImportExportCV/Hercules.ED.ImportExportCV/Controllers/ImportadorCVController.cs
@@ -16,6 +16,7 @@ using Gnoss.ApiWrapper.Model;
 using Models;
 using ImportadorWebCV.Exporta.Secciones;
 using Gnoss.ApiWrapper;
+using ImportadorWebCV;
 
 namespace Hercules.ED.ImportExportCV.Controllers
 {
@@ -34,6 +35,16 @@ namespace Hercules.ED.ImportExportCV.Controllers
             System.Globalization.CultureInfo.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
             _logger = logger;
             _Configuracion = pConfig;
+        }
+
+        [HttpGet("LecturaDatos")]
+        public ActionResult lecturaDatos()
+        {
+        }
+
+        [HttpGet("LecturaCarpeta")]
+        public ActionResult lecturaCarpeta()
+        {
         }
 
         /// <summary>
