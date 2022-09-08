3e2e4acaaf491c6cff922d6775ce48231a036d42
danijmj <danijmj@hotmail.com>
2022-09-08 13:29:53 +0200

    Añadidas funciones para eliminar los caracteres extraños de un string y formatearlo como una url

--- Web/Estilos/theme/community_proy.js
+++ Web/Estilos/theme/community_proy.js
@@ -2185,3 +2185,28 @@ comportamientoVerMasVerMenosTags.comportamiento = function() {
         }
     });
 };
+
+
+/**
+ * Función que limpia un string como un enlace
+ */
+function cleanStringUrl (text) {
+	return removeAccents(text).toLowerCase().replace(/[^a-z0-9]+/g,'-')
+}
+
+/**
+ * Función que elimina los acentos
+ */
+function removeAccents (text) {
+	var ts = '';
+	for (var i = 0; i < texto.length; i++) {
+		var c = texto.charCodeAt(i);
+		if (c >= 224 && c <= 230) { ts += 'a'; }
+		else if (c >= 232 && c <= 235) { ts += 'e'; }
+		else if (c >= 236 && c <= 239) { ts += 'i'; }
+		else if (c >= 242 && c <= 246) { ts += 'o'; }
+		else if (c >= 249 && c <= 252) { ts += 'u'; }
+		else { ts += texto.charAt(i); }
+	}
+	return ts;
+}
