a5b4a71e6a99aafdd27cea0b76700c18624f8a19
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2022-09-09 10:10:55 +0200

    Update ActualizadorNotification.cs

--- src/Hercules.ED.Desnormalizador/Models/Actualizadores/ActualizadorNotification.cs
+++ src/Hercules.ED.Desnormalizador/Models/Actualizadores/ActualizadorNotification.cs
@@ -21,10 +21,7 @@ namespace DesnormalizadorHercules.Models.Actualizadores
 
         public void ActualizarNotificaciones()
         {
-            List<string> listResult = new List<string>();
-
-            int limit = 10000;
-            int offset = 0;
+            int limit = 500;
             //Fecha en formato GNOSS de hace 7 días.
             string dateNow = DateTime.Now.AddDays(-7).ToString("yyyyMMdd");
             dateNow += "000000";
@@ -33,56 +30,33 @@ namespace DesnormalizadorHercules.Models.Actualizadores
                 //Consulta.
                 string select = "SELECT distinct ?s ";
                 string where = $@"WHERE {{
-                                SELECT *
-                                WHERE{{
-						?s a <http://w3id.org/roh/Notification> . 
-						?s <http://purl.org/dc/terms/issued> ?o . FILTER(xsd:integer(?o) < {dateNow} )
-					}}order by (?o) }}limit {limit} offset {offset}";
+						                ?s a <http://w3id.org/roh/Notification> . 
+						                ?s <http://purl.org/dc/terms/issued> ?o . 
+                                        FILTER(xsd:long(?o) < {dateNow} )
+					                }}limit {limit}";
 
                 SparqlObject resultData = mResourceApi.VirtuosoQuery(select, where, "notification");
-                foreach (Dictionary<string, Data> fila in resultData.results.bindings)
+
+                Parallel.ForEach(resultData.results.bindings, new ParallelOptions { MaxDegreeOfParallelism = 5 }, fila =>
                 {
-                    listResult.Add(fila["s"].value);
-                }
-                offset += limit;
+
+                    //Eliminar el recurso
+                    try
+                    {
+                        mResourceApi.PersistentDelete(mResourceApi.GetShortGuid(fila["s"].value));
+
+                    }
+                    catch (Exception e)
+                    {
+                        mResourceApi.Log.Error(e.Message);
+                    }
+                });
 
                 if (resultData.results.bindings.Count < limit)
                 {
                     break;
                 }
             }
-
-
-            //Borra los recursos.
-            Parallel.ForEach(listResult, new ParallelOptions { MaxDegreeOfParallelism = 5 }, identificador =>
-            {
-                int numIntentos = 0;
-                while (true)
-                {
-                    numIntentos++;
-                    //Si el número de intentos es superior a 10 no continuo intentando eliminar el recurso, e inserto en el log el identificador.
-                    if (numIntentos > 10)
-                    {
-                        mResourceApi.Log.Error("El recurso " + identificador + " no se ha podido eliminar");
-                        break;
-                    }
-
-                    //Eliminar el recurso
-                    try
-                    {
-                        if (mResourceApi.PersistentDelete(mResourceApi.GetShortGuid(identificador)))
-                        {
-                            //Si elimino el recurso salgo del bucle.
-                            break;
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        mResourceApi.Log.Error(e.Message);
-                    }
-                }
-            });
-
         }
     }
 }
