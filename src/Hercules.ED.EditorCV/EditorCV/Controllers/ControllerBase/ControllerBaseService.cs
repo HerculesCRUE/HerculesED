7e57ebaf3cd656cd622d3d908bb45751c9db411d
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2022-09-12 17:28:06 +0200

    Create ControllerBaseService.cs

---
+++ src/Hercules.ED.EditorCV/EditorCV/Controllers/ControllerBase/ControllerBaseService.cs
@@ -0,0 +1,28 @@
+﻿using Gnoss.ApiWrapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EditorCV.Controllers
+{
+    public abstract class ControllerBaseService : ControllerBase
+    {
+        private static readonly Gnoss.ApiWrapper.UserApi mUserApi = new Gnoss.ApiWrapper.UserApi($@"{System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase}Config/ConfigOAuth/OAuthV3.config");
+        protected bool HavePermission(HttpRequest pRequest, Guid pIdUsuario)
+        {
+            try
+            {
+                Guid userID = mUserApi.GetUserIDFromCookie(Request.Cookies["_UsuarioActual"]);
+                return pIdUsuario == userID;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
