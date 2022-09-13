3a5a3b0d9d114cfa66471f9b2e115be259218ae8
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2022-09-13 12:23:31 +0200

    Create Security.cs

---
+++ src/Hercules.ED.EditorCV/EditorCV/Models/Security.cs
@@ -0,0 +1,26 @@
+﻿using Gnoss.ApiWrapper;
+using System;
+using Microsoft.AspNetCore.Http;
+
+namespace EditorCV.Models
+{
+    public static class Security
+    {
+        static UserApi mUserApi = new UserApi($@"{System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase}Config/ConfigOAuth/OAuthV3.config");
+
+        public static bool CheckUser(Guid pUserId,HttpRequest pHttpRequest)
+        {
+            string cookie = pHttpRequest.Cookies["_UsuarioActual"];
+            Guid userIdCookie = Guid.Empty;
+            try
+            {
+                userIdCookie = mUserApi.GetUserIDFromCookie(cookie);
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            return userIdCookie==pUserId;
+        }
+    }
+}
