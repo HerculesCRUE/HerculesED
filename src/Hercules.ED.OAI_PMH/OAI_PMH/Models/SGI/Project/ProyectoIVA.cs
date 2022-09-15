b924d7944d11340c9c31877cc291fc1367bbf6fb
ManuelGNOSS <manuelboillos@gnoss.com>
2022-09-15 17:14:58 +0200

    Comentarios

--- src/Hercules.ED.OAI_PMH/OAI_PMH/Models/SGI/Project/ProyectoIVA.cs
+++ src/Hercules.ED.OAI_PMH/OAI_PMH/Models/SGI/Project/ProyectoIVA.cs
@@ -6,16 +6,46 @@ using System.Threading.Tasks;
 
 namespace OAI_PMH.Models.SGI.Project
 {
+    /// <summary>
+    /// Proyecto IVA
+    /// </summary>
     public class ProyectoIVA : SGI_Base
     {
-        public string CreatedBy { get; set; }
-        public string CreationDate { get; set; }
-        public string LastModifiedBy { get; set; }
-        public string LastModifiedDate { get; set; }
+        /// <summary>
+        /// Identificador del tipo de finalidad.
+        /// </summary>
         public string Id { get; set; }
+        /// <summary>
+        /// Nombre del usuario que ha creado la entidad.
+        /// </summary>
+        public string CreatedBy { get; set; }
+        /// <summary>
+        /// Fecha de la creación de la entidad.
+        /// </summary>
+        public string CreationDate { get; set; }
+        /// <summary>
+        /// Nombre del usuario que ha modificado por última vez la entidad.
+        /// </summary>
+        public string LastModifiedBy { get; set; }
+        /// <summary>
+        /// Fecha de la última modificación de la entidad.
+        /// </summary>
+        public string LastModifiedDate { get; set; }
+        /// <summary>
+        /// Identificador del proyecto
+        /// </summary>
         public string ProyectoId { get; set; }
+        /// <summary>
+        /// Valor del porcentaje del IVA.
+        /// </summary>
         public double? Iva { get; set; }
+        /// <summary>
+        /// Fecha de inicio en la que empieza a aplicarse el porcentaje de IVA al que refiere el registro.
+        /// </summary>
         public string FechaInicio { get; set; }
+        /// <summary>
+        /// Fecha de fin en la que deja de aplicarse el porcentaje de IVA al que refiere el registro.
+        /// </summary>
         public string FechaFin { get; set; }
     }
 }
--- src/Hercules.ED.OAI_PMH/OAI_PMH/Models/SGI/Project/ProyectoPresupuestoTotales.cs
+++ src/Hercules.ED.OAI_PMH/OAI_PMH/Models/SGI/Project/ProyectoPresupuestoTotales.cs
@@ -1,14 +1,45 @@
 ﻿namespace OAI_PMH.Models.SGI.Project
 {
+    /// <summary>
+    /// Proyecto presupuesto totales
+    /// </summary>
     public class ProyectoPresupuestoTotales : SGI_Base
     {
+        /// <summary>
+        /// Importe total (suma del importe de todas las anualidades) presupuestado a la Universidad para el desarrollo del proyecto,
+        /// asociado a conceptos de gasto que no representan costes indirectos.
+        /// </summary>
         public float importeTotalPresupuestoUniversidadSinCosteIndirecto { get; set; }
+        /// <summary>
+        /// Importe total (suma del importe de todas las anualidades) presupuestado al resto de socios de la Universidad para el desarrollo del proyecto.
+        /// </summary>
         public float importeTotalPresupuestoSocios { get; set; }
+        /// <summary>
+        /// Importe total (suma del importe de todas las anualidades) concedido a la Universidad para el desarrollo del proyecto, 
+        /// asociado a conceptos de gasto que no representan costes indirectos.
+        /// </summary>
         public float importeTotalConcedidoUniversidadSinCosteIndirecto { get; set; }
+        /// <summary>
+        /// Importe total (suma del importe de todas las anualidades) concedido al resto de socios de la Universidad para el desarrollo del proyecto.
+        /// </summary>
         public float importeTotalConcedidoSocios { get; set; }
+        /// <summary>
+        /// Importe total (suma del importe de todas las anualidades) presupuestado.
+        /// </summary>
         public float importeTotalPresupuesto { get; set; }
+        /// <summary>
+        /// Importe total (suma del importe de todas las anualidades) concedido.
+        /// </summary>
         public float importeTotalConcedido { get; set; }
+        /// <summary>
+        /// Importe total (suma del importe de todas las anualidades) presupuestado a la Universidad para el desarrollo del proyecto,
+        /// asociado a conceptos de gasto que representan costes indirectos.
+        /// </summary>
         public float importeTotalPresupuestoUniversidadCostesIndirectos { get; set; }
+        /// <summary>
+        /// Importe total (suma del importe de todas las anualidades) concedido a la Universidad para el desarrollo del proyecto,
+        /// asociado a conceptos de gasto que representan costes indirectos.
+        /// </summary>
         public float importeTotalConcedidoUniversidadCostesIndirectos { get; set; }
     }
 }
