c923b58e3a4558d56d856ac82dc6c1d150f2366d
ManuelGNOSS <manuelboillos@gnoss.com>
2022-09-15 13:00:04 +0200

    Comentarios

---
+++ src/Hercules.ED.OAI_PMH/OAI_PMH/Models/SGI/ActividadDocente/TipoEvento.cs
@@ -0,0 +1,8 @@
+﻿namespace OAI_PMH.Models.SGI.ActividadDocente
+{
+    public class TipoEvento : SGI_Base
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
--- src/Hercules.ED.OAI_PMH/OAI_PMH/Models/SGI/OrganicStructure/TipoTitulacion.cs
+++ src/Hercules.ED.OAI_PMH/OAI_PMH/Models/SGI/OrganicStructure/TipoTitulacion.cs
@@ -2,7 +2,13 @@
 {
     public class TipoTitulacion : SGI_Base
     {
+        /// <summary>
+        /// Identificador
+        /// </summary>
         public string Id { get; set; }
+        /// <summary>
+        /// Nombre
+        /// </summary>
         public string Nombre { get; set; }
     }
 }
