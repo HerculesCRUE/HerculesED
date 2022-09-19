a1b07accd10215a1258dc7dbb70ae492a3016743
ManuelGNOSS <manuelboillos@gnoss.com>
2022-09-19 11:50:17 +0200

    Update AccionesEnvioDSpace.cs

--- src/Hercules.ED.EditorCV/EditorCV/Models/AccionesEnvioDSpace.cs
+++ src/Hercules.ED.EditorCV/EditorCV/Models/AccionesEnvioDSpace.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Security.Policy;
 using System.Text;
 
 namespace EditorCV.Models
@@ -22,12 +23,29 @@ namespace EditorCV.Models
             //Compruebo si está el ítem en la biblioteca
             try
             {
+                Status status = new Status();
                 string urlStatus = _Configuracion.GetUrlDSpace() + "/status";
                 HttpClient httpClientStatus = new HttpClient();
                 HttpResponseMessage responseStatus = httpClientStatus.GetAsync($"{urlStatus}").Result;
                 if (responseStatus.IsSuccessStatusCode)
                 {
-                    Status s = responseStatus.Content.ReadFromJsonAsync<Status>().Result;
+                    status = responseStatus.Content.ReadFromJsonAsync<Status>().Result;
+                    if (status.okay != "true")
+                    {
+                        throw new Exception("Status not okay");
+                    }
+                }
+
+                //Compruebo si estoy autenticado
+                if (status.authenticated != "true")
+                {
+                    string urlLogin = _Configuracion.GetUrlDSpace() + "/login";
+                    List<KeyValuePair<string, string>> valores = new List<KeyValuePair<string, string>>();
+                    valores.Add(new KeyValuePair<string, string>("email", _Configuracion.GetUsernameDspace()));
+                    valores.Add(new KeyValuePair<string, string>("password", _Configuracion.GetPasswordDspace()));
+                    HttpClient httpClientLoguin = new HttpClient();
+                    HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Post, urlLogin) { Content = new FormUrlEncodedContent(valores) };
+                    HttpResponseMessage res = httpClientLoguin.SendAsync(req).Result;
                 }
 
                 string urlEstado = _Configuracion.GetUrlDSpace() + "/items/" + pIdRecurso;
