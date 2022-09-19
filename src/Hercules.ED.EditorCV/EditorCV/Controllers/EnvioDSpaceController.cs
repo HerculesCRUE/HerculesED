7a39b0b21c798dbbee940cb4b12ad8a04318408b
ManuelGNOSS <manuelboillos@gnoss.com>
2022-09-19 08:50:01 +0200

    Creación controlador Dspace

---
+++ src/Hercules.ED.EditorCV/EditorCV/Controllers/EnvioDSpaceController.cs
@@ -0,0 +1,62 @@
+﻿using EditorCV.Models;
+using EditorCV.Models.Utils;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace EditorCV.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [EnableCors("_myAllowSpecificOrigins")]
+    public class EnvioDSpaceController: ControllerBase
+    {
+        readonly ConfigService _Configuracion;
+
+        public EnvioDSpaceController(ConfigService pConfig)
+        {
+            _Configuracion = pConfig;
+        }
+
+        [HttpPost("EnvioDSpace")]
+        public IActionResult EnvioDSpace([FromForm][Required] string pIdRecurso, [FromForm] List<string> pIdProyecto)
+        {
+            try
+            {
+                if (!Security.CheckUsers(UtilityCV.GetUsersFromDocument(pIdRecurso), Request))
+                {
+                    return StatusCode(StatusCodes.Status401Unauthorized);
+                }
+                AccionesEnvioDSpace accionesEnvioDSpace = new AccionesEnvioDSpace(_Configuracion);
+            }
+            catch (Exception e)
+            {
+                return Problem(e.Message);
+            }
+
+            return Ok();
+        }
+
+        [HttpPut("EnvioDSpace")]
+        public IActionResult ActualizarDSpace([FromForm][Required] string pIdRecurso, [FromForm] List<string> pIdProyecto)
+        {
+            try
+            {
+                if (!Security.CheckUsers(UtilityCV.GetUsersFromDocument(pIdRecurso), Request))
+                {
+                    return StatusCode(StatusCodes.Status401Unauthorized);
+                }
+                AccionesEnvioDSpace accionesEnvioDSpace = new AccionesEnvioDSpace(_Configuracion);
+            }
+            catch (Exception e)
+            {
+                return Problem(e.Message);
+            }
+
+            return Ok();
+        }
+    }
+}
