bee1519ae3d90a80f2578737746a0c7a44eeb3e1
ManuelGNOSS <manuelboillos@gnoss.com>
2022-09-19 09:59:52 +0200

    Update AccionesEnvioDSpace.cs

--- src/Hercules.ED.EditorCV/EditorCV/Models/AccionesEnvioDSpace.cs
+++ src/Hercules.ED.EditorCV/EditorCV/Models/AccionesEnvioDSpace.cs
@@ -1,4 +1,8 @@
-﻿namespace EditorCV.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace EditorCV.Models
 {
     public class AccionesEnvioDSpace
     {
@@ -11,6 +15,31 @@
 
         public void EnvioDSpace(ConfigService pConfig, string pIdRecurso)
         {
+            //Compruebo si está el ítem en la biblioteca
+            try
+            {
+                string urlEstado = _Configuracion.GetUrlDSpace() + "/items/" + pIdRecurso;
+                HttpClient httpClientEstado = new HttpClient();
+                HttpResponseMessage responseEstado = httpClientEstado.GetAsync($"{urlEstado}").Result;
+                if (responseEstado.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    //En caso de no estar lo inserto
+
+                }
+                else if (responseEstado.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    //Si está en la biblioteca actualizo los datos
+
+                }
+                else
+                {
+                    throw new Exception("No se ha recibido un código de estado válido");
+                }
+            }
+            catch (Exception)
+            {
+
+            }
         }
 
 
