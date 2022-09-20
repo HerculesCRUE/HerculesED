d1dc1f3f318a89440d21f289b1b11b036609cbf2
Mario Heras <marioheras@gnoss.com>
2022-09-20 09:48:31 +0200

    Update GrupoEquipo.cs

--- src/Hercules.ED.Harvester/Harvester/Models/SGI/Grupos/GrupoEquipo.cs
+++ src/Hercules.ED.Harvester/Harvester/Models/SGI/Grupos/GrupoEquipo.cs
@@ -2,13 +2,13 @@
 {
     public class GrupoEquipo : SGI_Base
     {
-        public int id { get; set; }
-        public int grupoId { get; set; }
+        public int? id { get; set; }
+        public int? grupoId { get; set; }
         public string personaRef { get; set; }
         public RolProyecto rol { get; set; }
         public string fechaInicio { get; set; }
         public string fechaFin { get; set; }
         public string dedicacion { get; set; }
-        public float participacion { get; set; }
+        public float? participacion { get; set; }
     }
 }
