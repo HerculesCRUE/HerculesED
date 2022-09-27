c5099a9d91da200b306d9c4c62cc9d12acbb6526
Adrián <88077103+AdrianSaavedra-GNOSS@users.noreply.github.com>
2022-09-27 16:49:16 +0200

    Update Harvester

--- src/Hercules.ED.Harvester/Harvester/Models/SGI/Grupos/Grupo.cs
+++ src/Hercules.ED.Harvester/Harvester/Models/SGI/Grupos/Grupo.cs
@@ -5,17 +5,69 @@ namespace OAI_PMH.Models.SGI.Grupos
 {
     public class Grupo : SGI_Base
     {
-        public long? id { get; set; }
+        /// <summary>
+        /// Identificador.
+        /// </summary>
+        public int? id { get; set; }
+        /// <summary>
+        /// Nombre del grupo.
+        /// </summary>
         public string nombre { get; set; }
+        /// <summary>
+        /// Feha de inicio del grupo.
+        /// </summary>
         public DateTime? fechaInicio { get; set; }
-        public DateTime? fechaFin { get;set; }
+        /// <summary>
+        /// Fecha de fin del grupo.
+        /// </summary>
+        public DateTime? fechaFin { get; set; }
+        /// <summary>
+        /// Identificador en el SGE.
+        /// </summary>
         public string proyectoSgeRef { get; set; }
-        public long? solicitudId { get; set; }
+        /// <summary>
+        /// ID de solicitud.
+        /// </summary>
+        public int? solicitudId { get; set; }
+        /// <summary>
+        /// Código del grupo.
+        /// </summary>
         public string codigo { get; set; }
+        /// <summary>
+        /// Tipo del grupo.
+        /// </summary>
         public string tipo { get; set; }
+        /// <summary>
+        /// Especial Investigacion
+        /// </summary>
         public bool? especialInvestigacion { get; set; }
+        /// <summary>
+        /// Indica si el grupo está acitvo o no.
+        /// </summary>
         public bool? activo { get; set; }
+        /// <summary>
+        /// Miembros del equipo del grupo.
+        /// </summary>
         public List<GrupoEquipo> equipo { get; set; }
-        public List<string> areasTematicas { get; set; }
+        /// <summary>
+        /// Palabras clave del grupo.
+        /// </summary>
+        public List<GrupoPalabraClave> palabrasClave { get; set; }
+        /// <summary>
+        /// Lineas de clasificacion del grupo.
+        /// </summary>
+        public List<LineaClasificacion> lineasClasificacion { get; set; }
+        /// <summary>
+        /// Lineas de investigación del grupo.
+        /// </summary>
+        public List<LineaInvestigacion> lineasInvestigacion { get; set; }
+        /// <summary>
+        /// Investigadores principales del grupo.
+        /// </summary>
+        public List<string> investigadoresPrincipales { get; set; }
+        /// <summary>
+        /// Investigadores que más han participado en el grupo.
+        /// </summary>
+        public List<string> investigadoresPrincipalesMaxParticipacion { get; set; }
     }
 }
---
+++ src/Hercules.ED.Harvester/Harvester/Models/SGI/Grupos/LineaClasificacion.cs
@@ -0,0 +1,17 @@
+﻿namespace OAI_PMH.Models.SGI.Grupos
+{ 
+    /// <summary>
+    /// LineaClasificacion
+    /// </summary>
+    public class LineaClasificacion : SGI_Base
+    {
+        /// <summary>
+        /// Id.
+        /// </summary>
+        public int? id { get; set; }
+        /// <summary>
+        /// Clasificación Ref.
+        /// </summary>
+        public string clasificacionRef { get; set; }
+    }
+}
