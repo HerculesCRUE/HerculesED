f926c7498112f05103b2afa293c67635e1445911
ManuelGNOSS <manuelboillos@gnoss.com>
2022-09-28 15:38:19 +0200

    Update AccionesEnvioDSpace.cs

--- src/Hercules.ED.EditorCV/EditorCV/Models/AccionesEnvioDSpace.cs
+++ src/Hercules.ED.EditorCV/EditorCV/Models/AccionesEnvioDSpace.cs
@@ -1,8 +1,14 @@
 ﻿using EditorCV.Models.EnvioDSpace;
 using EditorCV.Models.PreimportModels;
+using Gnoss.ApiWrapper;
+using Gnoss.ApiWrapper.ApiModel;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Security.Policy;
 using System.Text;
