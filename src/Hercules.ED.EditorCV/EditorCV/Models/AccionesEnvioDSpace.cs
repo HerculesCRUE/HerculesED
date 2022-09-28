115e08a2f66ad5adad743f7549b1b23cc5ec97a3
ManuelGNOSS <manuelboillos@gnoss.com>
2022-09-28 15:38:13 +0200

    Update AccionesEnvioDSpace.cs

--- src/Hercules.ED.EditorCV/EditorCV/Models/AccionesEnvioDSpace.cs
+++ src/Hercules.ED.EditorCV/EditorCV/Models/AccionesEnvioDSpace.cs
@@ -13,6 +13,12 @@ namespace EditorCV.Models
     {
         readonly ConfigService _Configuracion;
 
+        /// <summary>
+        /// API
+        /// </summary>
+        private static readonly ResourceApi mResourceApi = new ResourceApi($@"{System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase}Config/ConfigOAuth/OAuthV3.config");
+        private static string tokenAuth = "";
+
         public AccionesEnvioDSpace(ConfigService pConfig)
         {
             _Configuracion = pConfig;
@@ -20,11 +26,17 @@ namespace EditorCV.Models
 
         public void EnvioDSpace(string pIdRecurso)
         {
-            //Compruebo si está el ítem en la biblioteca
+            string idRecursoDspace = "000000";
+            string tituloRecurso = "";
             try
             {
                 //Compruebo el estado del servicio
                 Status status = GetStatus();
+
+                if (status.okay != "true")
+                {
+                    throw new Exception("El servicio no está operativo");
+                }
 
                 //Compruebo si estoy autenticado
                 if (status.authenticated != "true")
@@ -32,19 +44,70 @@ namespace EditorCV.Models
                     Authentication();
                 }
 
-                //Inserto o actualizo
-                string urlEstado = _Configuracion.GetUrlDSpace() + "/items/" + pIdRecurso;
+                //Recupero el identificador de Dspace y el titulo del recurso de la BBDD.
+                string select = "SELECT distinct ?doc ?idDspace ?titulo";
+                string where = $@"WHERE{{
+                                    ?doc a <http://purl.org/ontology/bibo/Document>.
+                                    <{pIdRecurso}> ?p ?doc .
+                                    OPTIONAL{{?doc <http://w3id.org/roh/idDspace> ?idDspace }}
+                                    OPTIONAL{{?doc <http://w3id.org/roh/title> ?titulo }}
+                                }}";
+                SparqlObject resultadoQuery = mResourceApi.VirtuosoQueryMultipleGraph(select, where, new List<string> { "document", "curriculumvitae" });
+                if (resultadoQuery.results.bindings.Count != 0)
+                {
+                    foreach (Dictionary<string, SparqlObject.Data> res in resultadoQuery.results.bindings)
+                    {
+                        if (res.ContainsKey("idDspace"))
+                        {
+                            idRecursoDspace = res["idDspace"].value.ToString();
+                        }
+                        if (res.ContainsKey("titulo"))
+                        {
+                            tituloRecurso = res["titulo"].value.ToString();
+                        }
+                    }
+                }
+
+                if (idRecursoDspace == "000000")
+                {
+                    //Buscar un recurso por el nombre
+                    string urlEstadoFind = _Configuracion.GetUrlDSpace() + "/items/find-by-metadata-field";
+                    HttpClient httpClientEstadoFind = new HttpClient();
+
+                    MetadataEntry camp = new MetadataEntry("dc.title", tituloRecurso, "es_ES");
+
+                    HttpResponseMessage responseEstadoFind = httpClientEstadoFind.PostAsJsonAsync($"{urlEstadoFind}", camp).Result;
+                    if (responseEstadoFind.Content.ReadAsStringAsync().Result != "[]")
+                    {
+                        Item[] itemRespuesta = JsonConvert.DeserializeObject<Item[]>(responseEstadoFind.Content.ReadAsStringAsync().Result);
+                        idRecursoDspace = itemRespuesta[0].id.ToString();
+                    }
+                }
+
+                //Compruebo si el ítem está en DSpace y traigo los datos. 
+                string urlEstado = _Configuracion.GetUrlDSpace() + "/items/" + idRecursoDspace;
                 HttpClient httpClientEstado = new HttpClient();
                 HttpResponseMessage responseEstado = httpClientEstado.GetAsync($"{urlEstado}").Result;
-                if (responseEstado.StatusCode == System.Net.HttpStatusCode.NotFound)
+
+
+                //Inserto o actualizo.
+                if (responseEstado.StatusCode == HttpStatusCode.NotFound)
                 {
+                    MetadataEntry metadataEntry = new MetadataEntry();
+
                     //En caso de no estar lo inserto
-
+                    HttpClient httpClientInserta = new HttpClient();
+                    httpClientInserta.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("token", tokenAuth);
+                    HttpResponseMessage responseInserta = httpClientInserta.PostAsJsonAsync($"{urlEstado}", metadataEntry).Result;
                 }
-                else if (responseEstado.StatusCode == System.Net.HttpStatusCode.OK)
+                else if (responseEstado.StatusCode == HttpStatusCode.OK)
                 {
-                    //Si está en la biblioteca actualizo los datos
+                    MetadataEntry[] metadataEntryArray = new MetadataEntry[15];
 
+                    //Si está en la biblioteca actualizo los datos
+                    HttpClient httpClientActualiza = new HttpClient();
+                    httpClientActualiza.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("token", tokenAuth);
+                    HttpResponseMessage responseActualiza = httpClientActualiza.PutAsJsonAsync($"{urlEstado}", metadataEntryArray).Result;
                 }
                 else
                 {
@@ -55,6 +118,12 @@ namespace EditorCV.Models
             {
 
             }
+        }
+
+        private static List<MetadataEntry> GetListadoValoresItem(Item item)
+        {
+            List<MetadataEntry> listadoValores = new List<MetadataEntry>();
+            return listadoValores;
         }
 
         private Status GetStatus()
