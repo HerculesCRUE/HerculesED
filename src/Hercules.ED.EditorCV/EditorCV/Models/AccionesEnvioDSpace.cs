9a2b7612c864a18ee6ab46b293d063fc93da121f
ManuelGNOSS <manuelboillos@gnoss.com>
2022-10-04 14:19:13 +0200

    EnvioDSpace

--- src/Hercules.ED.EditorCV/EditorCV/Models/AccionesEnvioDSpace.cs
+++ src/Hercules.ED.EditorCV/EditorCV/Models/AccionesEnvioDSpace.cs
@@ -2,10 +2,12 @@
 using EditorCV.Models.PreimportModels;
 using Gnoss.ApiWrapper;
 using Gnoss.ApiWrapper.ApiModel;
+using Gnoss.ApiWrapper.Model;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -32,8 +34,9 @@ namespace EditorCV.Models
 
         public void EnvioDSpace(string pIdRecurso)
         {
-            string idRecursoDspace = "000000";
-            string tituloRecurso = "";
+            Publication publication = new Publication("000000", "");
+            string idRecurso = "";
+
             try
             {
                 //Compruebo el estado del servicio
@@ -51,74 +54,168 @@ namespace EditorCV.Models
                 }
 
                 //Recupero el identificador de Dspace y el titulo del recurso de la BBDD.
-                string select = "SELECT distinct ?doc ?idDspace ?titulo";
+                string select = "SELECT distinct ?doc ?idDspace ?titulo ?anioPublicacion ?issn ?isbn ?handle ?descripcion ?pagIni ?pagFin ?editorial ?isOpenAccess ?idTipo";
                 string where = $@"WHERE{{
                                     ?doc a <http://purl.org/ontology/bibo/Document>.
                                     <{pIdRecurso}> ?p ?doc .
                                     OPTIONAL{{?doc <http://w3id.org/roh/idDspace> ?idDspace }}
                                     OPTIONAL{{?doc <http://w3id.org/roh/title> ?titulo }}
+
+                                    OPTIONAL{{?doc <http://w3id.org/roh/year> ?anioPublicacion}}
+                                    OPTIONAL{{?doc <http://purl.org/ontology/bibo/issn> ?issn}}
+                                    OPTIONAL{{?doc <http://w3id.org/roh/isbn> ?isbn}}
+                                    OPTIONAL{{?doc <http://purl.org/ontology/bibo/handle> ?handle}}
+                                    OPTIONAL{{?doc <http://purl.org/ontology/bibo/abstract> ?descripcion}}
+                                    OPTIONAL{{?doc <http://purl.org/ontology/bibo/pageStart> ?pagIni}}
+                                    OPTIONAL{{?doc <http://purl.org/ontology/bibo/pageEnd> ?pagFin}}
+                                    OPTIONAL{{?doc <http://purl.org/ontology/bibo/publisher> ?editorial}}
+                                    OPTIONAL{{?doc <http://w3id.org/roh/openAccess> ?isOpenAccess}}
+                                    OPTIONAL{{?doc <http://purl.org/dc/elements/1.1/type> ?tipo . ?tipo <http://purl.org/dc/elements/1.1/identifier> ?idTipo}}
                                 }}";
-                SparqlObject resultadoQuery = mResourceApi.VirtuosoQueryMultipleGraph(select, where, new List<string> { "document", "curriculumvitae" });
+                SparqlObject resultadoQuery = mResourceApi.VirtuosoQueryMultipleGraph(select, where, new List<string> { "curriculumvitae", "document", "publicationtype" });
                 if (resultadoQuery.results.bindings.Count != 0)
                 {
                     foreach (Dictionary<string, SparqlObject.Data> res in resultadoQuery.results.bindings)
                     {
-                        if (res.ContainsKey("idDspace"))
+                        if (res.ContainsKey("doc") && !string.IsNullOrEmpty(res["doc"].value))
                         {
-                            idRecursoDspace = res["idDspace"].value.ToString();
+                            idRecurso = res["doc"].value;
                         }
-                        if (res.ContainsKey("titulo"))
+
+                        if (res.ContainsKey("idDspace") && !string.IsNullOrEmpty(res["idDspace"].value))
                         {
-                            tituloRecurso = res["titulo"].value.ToString();
+                            publication.idRecursoDspace = res["idDspace"].value.ToString();
+                        }
+                        if (res.ContainsKey("titulo") && !string.IsNullOrEmpty(res["titulo"].value))
+                        {
+                            publication.tituloRecurso = res["titulo"].value.ToString();
+                        }
+
+                        if (res.ContainsKey("anioPublicacion") && !string.IsNullOrEmpty(res["anioPublicacion"].value))
+                        {
+                            publication.anioPublicacion = res["anioPublicacion"].value.ToString();
+                        }
+                        if (res.ContainsKey("issn") && !string.IsNullOrEmpty(res["issn"].value))
+                        {
+                            publication.issn = res["issn"].value.ToString();
+                        }
+                        if (res.ContainsKey("isbn") && !string.IsNullOrEmpty(res["isbn"].value))
+                        {
+                            publication.isbn = res["isbn"].value.ToString();
+                        }
+                        if (res.ContainsKey("handle") && !string.IsNullOrEmpty(res["handle"].value))
+                        {
+                            publication.handle = res["handle"].value.ToString();
+                        }
+                        if (res.ContainsKey("descripcion") && !string.IsNullOrEmpty(res["descripcion"].value))
+                        {
+                            publication.descripcion = res["descripcion"].value.ToString();
+                        }
+                        if (res.ContainsKey("pagIni") && !string.IsNullOrEmpty(res["pagIni"].value))
+                        {
+                            publication.pagIni = res["pagIni"].value.ToString();
+                        }
+                        if (res.ContainsKey("pagFin") && !string.IsNullOrEmpty(res["pagFin"].value))
+                        {
+                            publication.pagFin = res["pagFin"].value.ToString();
+                        }
+                        if (res.ContainsKey("editorial") && !string.IsNullOrEmpty(res["editorial"].value))
+                        {
+                            publication.editorial = res["editorial"].value.ToString();
+                        }
+                        if (res.ContainsKey("isOpenAccess") && !string.IsNullOrEmpty(res["isOpenAccess"].value))
+                        {
+                            publication.isOpenAccess = res["isOpenAccess"].value.ToString();
+                        }
+                        if (res.ContainsKey("idTipo") && !string.IsNullOrEmpty(res["idTipo"].value))
+                        {
+                            publication.tipo = res["idTipo"].value.ToString();
                         }
                     }
                 }
 
-                if (idRecursoDspace == "000000")
+                //Autores del documento
+                string selectAutores = "select distinct ?nombrePersona";
+                string whereAutores = $@"WHERE{{
+                                    ?doc a <http://purl.org/ontology/bibo/Document>.
+                                    <{pIdRecurso}> ?p ?doc .
+                                    OPTIONAL{{
+                                        ?doc <http://purl.org/ontology/bibo/authorList> ?autores .
+                                        ?autores <http://www.w3.org/1999/02/22-rdf-syntax-ns#member> ?persona . 
+                                        ?persona <http://xmlns.com/foaf/0.1/name> ?nombrePersona .
+                                    }}
+                                }}";
+                SparqlObject resultadoAutores = mResourceApi.VirtuosoQueryMultipleGraph(selectAutores, whereAutores, new List<string> { "person", "document", "curriculumvitae" });
+                if (resultadoAutores.results.bindings.Count != 0)
+                {
+                    foreach (Dictionary<string, SparqlObject.Data> res in resultadoAutores.results.bindings)
+                    {
+                        if (res.ContainsKey("nombrePersona") && !string.IsNullOrEmpty(res["nombrePersona"].value))
+                        {
+                            publication.autores.Add(res["nombrePersona"].value);
+                        }
+                    }
+                }
+
+                if (publication.idRecursoDspace == "000000")
                 {
                     //Buscar un recurso por el nombre
                     string urlEstadoFind = _Configuracion.GetUrlDSpace() + "/items/find-by-metadata-field";
                     HttpClient httpClientEstadoFind = new HttpClient();
 
-                    MetadataEntry camp = new MetadataEntry("dc.title", tituloRecurso, "es_ES");
-
-                    HttpResponseMessage responseEstadoFind = httpClientEstadoFind.PostAsJsonAsync($"{urlEstadoFind}", camp).Result;
-                    if (responseEstadoFind.Content.ReadAsStringAsync().Result != "[]")
+                    Metadata camp = new Metadata("dc.title", publication.tituloRecurso);
+                    try
                     {
-                        Item[] itemRespuesta = JsonConvert.DeserializeObject<Item[]>(responseEstadoFind.Content.ReadAsStringAsync().Result);
-                        idRecursoDspace = itemRespuesta[0].id.ToString();
+                        HttpResponseMessage responseEstadoFind = httpClientEstadoFind.PostAsJsonAsync($"{urlEstadoFind}", camp).Result;
+                        if (responseEstadoFind.Content.ReadAsStringAsync().Result != "[]")
+                        {
+                            Item[] itemRespuesta = JsonConvert.DeserializeObject<Item[]>(responseEstadoFind.Content.ReadAsStringAsync().Result);
+                            publication.idRecursoDspace = itemRespuesta[0].id.ToString();
+                        }
+                    }
+                    catch (Exception)
+                    {
+
                     }
                 }
 
                 //Compruebo si el ítem está en DSpace y traigo los datos. 
-                string urlEstado = _Configuracion.GetUrlDSpace() + "/items/" + idRecursoDspace;
+                string urlEstado = _Configuracion.GetUrlDSpace() + "/items/" + publication.idRecursoDspace;
                 HttpClient httpClientEstado = new HttpClient();
+                httpClientEstado.DefaultRequestHeaders.Add("rest-dspace-token", tokenAuth);
                 HttpResponseMessage responseEstado = httpClientEstado.GetAsync($"{urlEstado}").Result;
 
 
-                //Inserto o actualizo.
+                //Inserto(404, recurso no encontrado por ID).
                 if (responseEstado.StatusCode == HttpStatusCode.NotFound)
                 {
                     //Colección con handle
                     urlEstado = _Configuracion.GetUrlDSpace() + "/collections/" + _Configuracion.GetCollectionDSpace() + "/items";
-                    MetadataEntry metadataEntry = new MetadataEntry("dc.title", tituloRecurso, "es_ES");
+                    MetadataSend metadata = GetListadoValoresItem(publication);
 
                     //En caso de no estar lo inserto
                     HttpClient httpClientInserta = new HttpClient();
                     httpClientInserta.DefaultRequestHeaders.Add("rest-dspace-token", tokenAuth);
-                    HttpResponseMessage responseInserta = httpClientInserta.PostAsJsonAsync($"{urlEstado}", metadataEntry).Result;
+                    HttpResponseMessage responseInserta = httpClientInserta.PostAsJsonAsync($"{urlEstado}", metadata.rootObject).Result;
+
+                    DSpaceResponse dSpace = JsonConvert.DeserializeObject<DSpaceResponse>(responseInserta.Content.ReadAsStringAsync().Result.ToString());
+
+                    //TODO - Inserta el triple con el Identificador de DSpace 
+                    AniadirIdDspace(dSpace.id, idRecurso);
                 }
+                //Actualizo(200, recurso encontrado en DSPACE)
                 else if (responseEstado.StatusCode == HttpStatusCode.OK)
                 {
-                    MetadataEntry[] metadataEntryArray = new MetadataEntry[15];
+                    MetadataSend metadata = GetListadoValoresItem(publication);
 
-
-                    urlEstado = _Configuracion.GetUrlDSpace() + "/collections/" + _Configuracion.GetCollectionDSpace() + "/items/" + idRecursoDspace;
+                    urlEstado = _Configuracion.GetUrlDSpace() + "/items/" + publication.idRecursoDspace + "/metadata";
 
                     //Si está en la biblioteca actualizo los datos
                     HttpClient httpClientActualiza = new HttpClient();
                     httpClientActualiza.DefaultRequestHeaders.Add("rest-dspace-token", tokenAuth);
-                    HttpResponseMessage responseActualiza = httpClientActualiza.PutAsJsonAsync($"{urlEstado}", metadataEntryArray).Result;
+                    HttpResponseMessage responseActualiza = httpClientActualiza.PutAsJsonAsync($"{urlEstado}", metadata.rootObject).Result;
+
+                    DSpaceResponse dSpace = JsonConvert.DeserializeObject<DSpaceResponse>(responseActualiza.Content.ReadAsStringAsync().Result.ToString());
                 }
                 else
                 {
@@ -131,7 +228,136 @@ namespace EditorCV.Models
             }
         }
 
-        private static List<MetadataEntry> GetListadoValoresItem(Item item)
+        /// <summary>
+        /// Añade el triple del identificador de DSpace al recurso <paramref name="pIdRecurso"/>
+        /// </summary>
+        /// <param name="IdDSpace">Identificador de DSpace</param>
+        /// <param name="pIdRecurso">Identificador largo del recuso</param>
+        private void AniadirIdDspace(string IdDSpace,string pIdRecurso)
+        {
+            //No hago nada si el identificador del recurso es nulo.
+            if (string.IsNullOrEmpty(pIdRecurso))
+            {
+                return;
+            }
+            //No hago nada si el identificador de DSpace es el valor por defecto.
+            if (IdDSpace.Equals("000000"))
+            {
+                return;
+            }
+
+            mResourceApi.ChangeOntoly("document");
+            Dictionary<Guid, List<TriplesToInclude>> dicInsercion = new Dictionary<Guid, List<TriplesToInclude>>();
+            List<TriplesToInclude> listaTriplesInsercion = new List<TriplesToInclude>();
+
+            //Triple de identificador de DSpace
+            TriplesToInclude triple = new TriplesToInclude();
+            triple.Predicate = "http://w3id.org/roh/idDspace";
+            triple.NewValue = IdDSpace;
+            listaTriplesInsercion.Add(triple);
+
+            dicInsercion.Add(mResourceApi.GetShortGuid(pIdRecurso), listaTriplesInsercion);
+            mResourceApi.InsertPropertiesLoadedResources(dicInsercion);
+        }
+
+        private static MetadataSend GetListadoValoresItem(Publication publication)
+        {
+            List<Metadata> listadoValores = new List<Metadata>();
+            //Titulo
+            if (!string.IsNullOrEmpty(publication.tituloRecurso))
+            {
+                Metadata metadataEntryTitle = new Metadata("dc.title", publication.tituloRecurso);
+                listadoValores.Add(metadataEntryTitle);
+            }
+
+            //Tipo de documento
+            if (!string.IsNullOrEmpty(publication.tipo))
+            {
+                Metadata metadataEntryType = new Metadata("dc.type", tipoType(publication.tipo));
+                listadoValores.Add(metadataEntryType);
+            }
+            
+            //Autores
+            if (publication.autores.Any())
+            {
+                foreach (string autor in publication.autores)
+                {
+                    Metadata metadataEntryAuthor = new Metadata("dc.contributor.author", autor);
+                    listadoValores.Add(metadataEntryAuthor);
+                }
+            }
+            
+            //Año de publicación
+            if (!string.IsNullOrEmpty(publication.anioPublicacion))
+            {
+                Metadata metadataEntryDateCreated = new Metadata("dc.date.created", publication.anioPublicacion);
+                listadoValores.Add(metadataEntryDateCreated);
+            }
+
+            //Fecha defensa/creación
+            //if (!string.IsNullOrEmpty(publication.)) { 
+            //MetadataEntry metadataEntryDateIssued = new MetadataEntry("dc.date.issued", "2022");
+            //listadoValores.Add(metadataEntryDateIssued);
+            //}
+
+            //Descripción
+            if (!string.IsNullOrEmpty(publication.descripcion))
+            {
+                Metadata metadataEntryDescription = new Metadata("dc.description", publication.descripcion);
+                listadoValores.Add(metadataEntryDescription);
+            }
+
+            //Resumen
+            //if (!string.IsNullOrEmpty(publication.)) { 
+            //MetadataEntry metadataEntryDescriptionAbstract = new MetadataEntry("dc.description.abstract", );
+            //listadoValores.Add(metadataEntryDescriptionAbstract);
+            //}
+
+            //ISBN
+            if (!string.IsNullOrEmpty(publication.isbn))
+            {
+                Metadata metadataEntryISBN = new Metadata("dc.identifier.isbn", publication.isbn);
+                listadoValores.Add(metadataEntryISBN);
+            }
+
+            //ISSN
+            if (!string.IsNullOrEmpty(publication.issn))
+            {
+                Metadata metadataEntryISSN = new Metadata("dc.identifier.issn", publication.issn);
+                listadoValores.Add(metadataEntryISSN);
+            }
+
+            //Handle
+            if (!string.IsNullOrEmpty(publication.handle))
+            {
+                Metadata metadataEntryIdentifierUri = new Metadata("dc.identifier.uri", publication.handle);
+                listadoValores.Add(metadataEntryIdentifierUri);
+            }
+
+            //Idioma
+            //if (!string.IsNullOrEmpty(publication.)) { 
+            //MetadataEntry metadataEntryLanguage = new MetadataEntry("dc.language", "");
+            //listadoValores.Add(metadataEntryLanguage);
+            //}
+
+            //Editorial
+            if (!string.IsNullOrEmpty(publication.editorial))
+            {
+                Metadata metadataEntryPublisher = new Metadata("dc.publisher", publication.editorial);
+                listadoValores.Add(metadataEntryPublisher);
+            }
+
+            MetadataSend metadataSend = new MetadataSend(listadoValores);
+
+            return metadataSend;
+        }
+
+        /// <summary>
+        /// Sustituye el valor numerico del tipo, por su equivalente de tipo de pueblocacion en openaire
+        /// </summary>
+        /// <param name="idTipo"></param>
+        /// <returns></returns>
+        private static string tipoType(string idTipo)
         {
             List<MetadataEntry> listadoValores = new List<MetadataEntry>();
             return listadoValores;
