a394dbf97224f3b1a4bb5daf2a6f318556d0832d
ManuelGNOSS <manuelboillos@gnoss.com>
2022-10-17 11:25:11 +0200

    Update AccionesSexenios.cs

--- src/Hercules.ED.EditorCV/EditorCV/Models/AccionesSexenios.cs
+++ src/Hercules.ED.EditorCV/EditorCV/Models/AccionesSexenios.cs
@@ -17,7 +17,6 @@ namespace EditorCV.Models
     public class AccionesSexenios
     {
         private static readonly ResourceApi mResourceApi = new ResourceApi($@"{System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase}Config/ConfigOAuth/OAuthV3.config");
-        private static readonly CommunityApi mCommunityApi = new CommunityApi($@"{System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase}Config/ConfigOAuth/OAuthV3.config");
 
         /// <summary>
         /// Envia una petición para conseguir los Sexenios del usuario
@@ -37,94 +36,39 @@ namespace EditorCV.Models
                 client.Timeout = new TimeSpan(1, 15, 0);
                 string urlSexenios = _Configuracion.GetUrlSGI() + "/api/orchestrator/schedules/execute";
 
-                string[] personaCrisIdentifier = ObtenerPersonaYCrisIdentifier(idInvestigador);
-                string persona = personaCrisIdentifier[0];
-                string crisIdentifier = personaCrisIdentifier[1];
-                ParameterSexenio sexenio = new ParameterSexenio(comite, periodo, perfil_tecnologico, subcomite, crisIdentifier);
+                string cvOf = Utils.UtilityCV.GetCVFromUser(idInvestigador);
+                string person = Utils.UtilityCV.GetPersonFromCV(cvOf);
+
+                //Consigo el identificador del investigador.
+                string investigador = Utils.UtilityCV.GetIdInvestigador(cvOf);
+
+                //Si no consigo ningún identificador del usuario.
+                if (string.IsNullOrEmpty(investigador))
+                {
+                    Utils.UtilityCV.EnvioNotificacion("NOTIFICACION_SEXENIOS_ERROR", person, "recuperarSexenios");
+                    throw new Exception("El usuario no se ha encontrado, id: " + idInvestigador);
+                }
+
+                ParameterSexenio sexenio = new ParameterSexenio(comite, periodo, perfil_tecnologico, subcomite, investigador);
                 string sexenioJson = JsonConvert.SerializeObject(sexenio);
-                if (sexenio.process.parameters.perfil_tecnologico == null)
-                {
-                    sexenioJson = sexenioJson.Replace(@$"""perfil_tecnologico"":null,", "");
-                }
-                if (sexenio.process.parameters.subcomite == null)
-                {
-                    sexenioJson = sexenioJson.Replace(@$"""subcomite"":null,", "");
-                }
+
                 StringContent content = new StringContent(sexenioJson, System.Text.Encoding.UTF8, "application/json");
 
                 HttpResponseMessage response = client.PostAsync($"{urlSexenios}", content).Result;
 
                 if (response.IsSuccessStatusCode)
                 {
-                    EnvioNotificacion("NOTIFICACION_SEXENIOS", persona);
+                    Utils.UtilityCV.EnvioNotificacion("NOTIFICACION_SEXENIOS", person, "recuperarSexenios");
                 }
                 else
                 {
-                    EnvioNotificacion("NOTIFICACION_SEXENIOS_ERROR", persona);
+                    Utils.UtilityCV.EnvioNotificacion("NOTIFICACION_SEXENIOS_ERROR", person, "recuperarSexenios");
                 }
-                response.EnsureSuccessStatusCode();
-            }
-            catch (Exception)
-            {
 
             }
-        }
-
-        /// <summary>
-        /// Método que obtiene el crisIdentifier y un recurso Person a partir de una id de una persona
-        /// </summary>
-        /// <param name="idInvestigador"></param>
-        /// <returns></returns>
-        public string[] ObtenerPersonaYCrisIdentifier(string idInvestigador)
-        {
-            string crisIdentifier = "";
-            string persona = "";
-            string select = "SELECT ?persona ?crisIdentifier";
-            string where = @$"WHERE {{
-                ?persona a <http://xmlns.com/foaf/0.1/Person>.
-                ?persona <http://w3id.org/roh/gnossUser> <http://gnoss/{idInvestigador.ToUpper()}>.
-                ?persona <http://w3id.org/roh/crisIdentifier> ?crisIdentifier.
-            }}";
-            SparqlObject resultadoQuery = mResourceApi.VirtuosoQuery(select, where, "person");
-            if (resultadoQuery != null && resultadoQuery.results != null && resultadoQuery.results.bindings != null && resultadoQuery.results.bindings.Count > 0)
+            catch (Exception ex)
             {
-                if (resultadoQuery.results.bindings.First().ContainsKey("crisIdentifier"))
-                {
-                    crisIdentifier = resultadoQuery.results.bindings.First()["crisIdentifier"].value;
-                }
-                if (resultadoQuery.results.bindings.First().ContainsKey("persona"))
-                {
-                    persona = resultadoQuery.results.bindings.First()["persona"].value;
-                }
-            }
-
-            return new string[] {persona, crisIdentifier};
-        }
-
-        /// <summary>
-        /// Método que envía una notificación de sexenios específica
-        /// </summary>
-        /// <param name="title"></param>
-        /// <param name="owner"></param>
-        public void EnvioNotificacion(string title, string owner)
-        {
-            Notification notificacion = new Notification();
-            notificacion.Roh_text = title;
-            notificacion.IdRoh_owner = owner;
-            notificacion.Dct_issued = DateTime.UtcNow;
-            notificacion.Roh_type = "recuperarSexenios";
-            mResourceApi.ChangeOntoly("notification");
-            ComplexOntologyResource recursoCargar = notificacion.ToGnossApiResource(mResourceApi);
-            int numIntentos = 0;
-            while (!recursoCargar.Uploaded)
-            {
-                numIntentos++;
-
-                if (numIntentos > 5)
-                {
-                    break;
-                }
-                mResourceApi.LoadComplexSemanticResource(recursoCargar);
+                mResourceApi.Log.Error(ex.Message);
             }
         }
 
