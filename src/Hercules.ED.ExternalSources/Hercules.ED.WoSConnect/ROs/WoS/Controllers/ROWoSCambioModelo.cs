f53bfccbc65b7af057474b9424d5983637af92d6
Mario Heras <marioheras@gnoss.com>
2022-10-25 08:35:24 +0200

    Update ROWoSCambioModelo.cs

--- src/Hercules.ED.ExternalSources/Hercules.ED.WoSConnect/ROs/WoS/Controllers/ROWoSCambioModelo.cs
+++ src/Hercules.ED.ExternalSources/Hercules.ED.WoSConnect/ROs/WoS/Controllers/ROWoSCambioModelo.cs
@@ -180,7 +180,7 @@ namespace WoSConnect.ROs.WoS.Controllers
         /// <returns>Título de la publicación.</returns>
         public string getTitle(PublicacionInicial pPublicacionIn)
         {
-            if (pPublicacionIn.static_data != null && pPublicacionIn.static_data.summary != null && pPublicacionIn.static_data.summary.titles != null && pPublicacionIn.static_data.summary.titles.title != null && pPublicacionIn.static_data.summary.titles.title.Any() && pPublicacionIn.static_data.summary.titles.title.FirstOrDefault(x => x.type == "item") != null && pPublicacionIn.static_data.summary.titles.title.FirstOrDefault(x => x.type == "item").content != null)
+            if (pPublicacionIn != null && pPublicacionIn.static_data != null && pPublicacionIn.static_data.summary != null && pPublicacionIn.static_data.summary.titles != null && pPublicacionIn.static_data.summary.titles.title != null && pPublicacionIn.static_data.summary.titles.title.Any())
             {
                 return pPublicacionIn.static_data.summary.titles.title.FirstOrDefault(x => x.type == "item").content;
             }
