9d789ab13123b31ca38325d76e3165ca031cf2fc
ManuelGNOSS <manuelboillos@gnoss.com>
2022-10-27 10:52:32 +0200

    Update ResultadosTecnologicos.cs

--- src/Hercules.ED.ImportExportCV/Hercules.ED.ImportExportCV/Sincro/Secciones/ExperienciaCientificaSubclases/ResultadosTecnologicos.cs
+++ src/Hercules.ED.ImportExportCV/Hercules.ED.ImportExportCV/Sincro/Secciones/ExperienciaCientificaSubclases/ResultadosTecnologicos.cs
@@ -13,36 +13,15 @@ namespace ImportadorWebCV.Sincro.Secciones.ExperienciaCientificaSubclases
         public string descripcion { get; set; }
         public string fecha { get; set; }
 
-        private static readonly DisambiguationDataConfig configDescripcion = new DisambiguationDataConfig()
-        {
-            type = DisambiguationDataConfigType.equalsTitle,
-            score = 0.8f
-        };
-
-        private static readonly DisambiguationDataConfig configFecha = new DisambiguationDataConfig()
-        {
-            type = DisambiguationDataConfigType.equalsItem,
-            score = 0.5f,
-            scoreMinus = 0.5f
-        };
+        private static readonly DisambiguationDataConfig configDescripcion = new(DisambiguationDataConfigType.equalsTitle, 0.8f);
+        private static readonly DisambiguationDataConfig configFecha = new(DisambiguationDataConfigType.equalsItem, 0.5f, 0.5f);
 
         public override List<DisambiguationData> GetDisambiguationData()
         {
-            List<DisambiguationData> data = new List<DisambiguationData>
+            List<DisambiguationData> data = new()
             {
-                new DisambiguationData()
-                {
-                    property = "descripcion",
-                    config = configDescripcion,
-                    value = descripcion
-                },
-
-                new DisambiguationData()
-                {
-                    property = "fecha",
-                    config = configFecha,
-                    value = fecha
-                }
+                new DisambiguationData(configDescripcion,"descripcion",descripcion),
+                new DisambiguationData(configFecha,"fecha",fecha)
             };
             return data;
         }
