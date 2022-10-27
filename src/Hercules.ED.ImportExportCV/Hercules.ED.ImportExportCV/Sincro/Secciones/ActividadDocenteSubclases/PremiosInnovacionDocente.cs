3433801800161e339d3e055b3200f10978c760f4
ManuelGNOSS <manuelboillos@gnoss.com>
2022-10-27 10:52:57 +0200

    Update PremiosInnovacionDocente.cs

--- src/Hercules.ED.ImportExportCV/Hercules.ED.ImportExportCV/Sincro/Secciones/ActividadDocenteSubclases/PremiosInnovacionDocente.cs
+++ src/Hercules.ED.ImportExportCV/Hercules.ED.ImportExportCV/Sincro/Secciones/ActividadDocenteSubclases/PremiosInnovacionDocente.cs
@@ -14,50 +14,17 @@ namespace ImportadorWebCV.Sincro.Secciones.ActividadDocenteSubclases
         public string fecha { get; set; }
         public string entidadConcesion { get; set; }
 
-        private static readonly DisambiguationDataConfig configNombre = new DisambiguationDataConfig()
-        {
-            type = DisambiguationDataConfigType.equalsTitle,
-            score = 0.8f
-        };
-
-        private static readonly DisambiguationDataConfig configFecha = new DisambiguationDataConfig()
-        {
-            type = DisambiguationDataConfigType.equalsItem,
-            score = 0.5f,
-            scoreMinus = 0.5f
-        };
-
-        private static readonly DisambiguationDataConfig configEC = new DisambiguationDataConfig()
-        {
-            type = DisambiguationDataConfigType.equalsItem,
-            score = 0.5f,
-            scoreMinus = 0.5f
-        };
+        private static readonly DisambiguationDataConfig configNombre = new(DisambiguationDataConfigType.equalsTitle, 0.8f);
+        private static readonly DisambiguationDataConfig configFecha = new(DisambiguationDataConfigType.equalsItem, 0.5f, 0.5f);
+        private static readonly DisambiguationDataConfig configEC = new(DisambiguationDataConfigType.equalsItem, 0.5f, 0.5f);
 
         public override List<DisambiguationData> GetDisambiguationData()
         {
-            List<DisambiguationData> data = new List<DisambiguationData>
+            List<DisambiguationData> data = new()
             {
-                new DisambiguationData()
-                {
-                    property = "descripcion",
-                    config = configNombre,
-                    value = nombre
-                },
-
-                new DisambiguationData()
-                {
-                    property = "fecha",
-                    config = configFecha,
-                    value = fecha
-                },
-
-                new DisambiguationData()
-                {
-                    property = "entidadConcesion",
-                    config = configEC,
-                    value = entidadConcesion
-                }
+                new DisambiguationData(configNombre,"descripcion",nombre),
+                new DisambiguationData(configFecha,"fecha",fecha),
+                new DisambiguationData(configEC, "entidadConcesion",entidadConcesion)
             };
             return data;
         }
