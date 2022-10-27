532b5576889e49d8f34fba37541bd5c2b42be65b
ManuelGNOSS <manuelboillos@gnoss.com>
2022-10-27 11:06:45 +0200

    Update AyudaBecas.cs

--- src/Hercules.ED.ImportExportCV/Hercules.ED.ImportExportCV/Sincro/Secciones/ActividadCientificaSubclases/AyudaBecas.cs
+++ src/Hercules.ED.ImportExportCV/Hercules.ED.ImportExportCV/Sincro/Secciones/ActividadCientificaSubclases/AyudaBecas.cs
@@ -14,50 +14,17 @@ namespace ImportadorWebCV.Sincro.Secciones.ActividadCientificaSubclases
         public string fecha { get; set; }
         public string entidadConcesionaria { get; set; }
 
-        private static readonly DisambiguationDataConfig configDescripcion = new DisambiguationDataConfig()
-        {
-            type = DisambiguationDataConfigType.equalsTitle,
-            score = 0.8f
-        };
-
-        private static readonly DisambiguationDataConfig configFecha = new DisambiguationDataConfig()
-        {
-            type = DisambiguationDataConfigType.equalsItem,
-            score = 0.5f,
-            scoreMinus = 0.5f
-        };
-        
-        private static readonly DisambiguationDataConfig configEC = new DisambiguationDataConfig()
-        {
-            type = DisambiguationDataConfigType.equalsItem,
-            score = 0.5f,
-            scoreMinus = 0.5f
-        };
+        private static readonly DisambiguationDataConfig configDescripcion = new(DisambiguationDataConfigType.equalsTitle, 0.8f);
+        private static readonly DisambiguationDataConfig configFecha = new(DisambiguationDataConfigType.equalsItem, 0.5f, 0.5f);
+        private static readonly DisambiguationDataConfig configEC = new(DisambiguationDataConfigType.equalsItem, 0.5f, 0.5f);
 
         public override List<DisambiguationData> GetDisambiguationData()
         {
-            List<DisambiguationData> data = new List<DisambiguationData>
+            List<DisambiguationData> data = new()
             {
-                new DisambiguationData()
-                {
-                    property = "descripcion",
-                    config = configDescripcion,
-                    value = descripcion
-                },
-
-                new DisambiguationData()
-                {
-                    property = "fecha",
-                    config = configFecha,
-                    value = fecha
-                },
-
-                new DisambiguationData()
-                {
-                    property = "entidadConcesionaria",
-                    config = configEC,
-                    value = entidadConcesionaria
-                }
+                new DisambiguationData(configDescripcion,"descripcion",descripcion),
+                new DisambiguationData(configFecha,"fecha",fecha),
+                new DisambiguationData(configEC,"entidadConcesionaria",entidadConcesionaria)
             };
             return data;
         }
@@ -97,8 +64,8 @@ namespace ImportadorWebCV.Sincro.Secciones.ActividadCientificaSubclases
                     {
                         ID = fila["item"].value,
                         descripcion = fila["itemTitle"].value,
-                        fecha = fila.ContainsKey("itemDate") ? fila["itemDate"].value : "" ,
-                        entidadConcesionaria = fila.ContainsKey("itemEC") ? fila["itemEC"].value : "" 
+                        fecha = fila.ContainsKey("itemDate") ? fila["itemDate"].value : "",
+                        entidadConcesionaria = fila.ContainsKey("itemEC") ? fila["itemEC"].value : ""
                     };
 
                     resultados.Add(pResourceApi.GetShortGuid(fila["item"].value).ToString(), ayudaBecas);
