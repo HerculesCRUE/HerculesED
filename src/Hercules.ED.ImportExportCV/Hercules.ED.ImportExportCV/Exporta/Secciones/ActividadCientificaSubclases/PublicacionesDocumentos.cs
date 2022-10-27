fd9e80e401f69aca557a567616b865d634f46764
ManuelGNOSS <manuelboillos@gnoss.com>
2022-10-27 11:26:10 +0200

    Update PublicacionesDocumentos.cs

--- src/Hercules.ED.ImportExportCV/Hercules.ED.ImportExportCV/Exporta/Secciones/ActividadCientificaSubclases/PublicacionesDocumentos.cs
+++ src/Hercules.ED.ImportExportCV/Hercules.ED.ImportExportCV/Exporta/Secciones/ActividadCientificaSubclases/PublicacionesDocumentos.cs
@@ -196,19 +196,6 @@ namespace ImportadorWebCV.Exporta.Secciones.ActividadCientificaSubclases
                 UtilityExportar.AddCitas(itemBean, dicCodigosScholar,
                     "060.010.010.310", keyValue.Value);
 
-                ////Cita Other
-                //List<Tuple<string, string>> citasother = UtilityExportar.GetCitasOther(Variables.ActividadCientificaTecnologica.pubDocumentosOtrasCitasNombre, Variables.ActividadCientificaTecnologica.pubDocumentosOtrasCitasNumero, keyValue.Value, mResourceApi);
-
-                //foreach (Tuple<string, string> citaOther in citasother)
-                //{
-                //    List<Tuple<string, string, string>> dicOtras = new List<Tuple<string, string, string>>();
-                //    dicOtras.Add(new Tuple<string, string, string>("Double", "060.010.010.310", citaOther.Item2));
-                //    dicOtras.Add(new Tuple<string, string, string>("String", "060.010.010.320", "OTHERS"));
-                //    dicOtras.Add(new Tuple<string, string, string>("String", "060.010.010.370", citaOther.Item1));
-                //    UtilityExportar.AddCitas(itemBean, dicOtras,
-                //        "060.010.010.310", keyValue.Value);
-                //}
-
                 //Índice de i
                 List<Tuple<string, string, string, string, string, string, string>> impactIndex = UtilityExportar.GetImpactIndex(keyValue.Value, mResourceApi);
                 foreach (Tuple<string, string, string, string, string, string, string> impactindex in impactIndex)
